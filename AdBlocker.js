19ee7a21e359cc79f6bcccd67ea655e93ec55d5c
RealRottale1 <camarkham2@gmail.com>
2024-10-10 18:05:51 -0400

    AA

--- AdBlocker.js
+++ AdBlocker.js
@@ -1,13 +1,14 @@
 
-function RemoveAds() {
-    let AllAds = []
-    AllAds.push(document.getElementsByTagName("iframe"))
+// Sets up MainDiv
+const MainDiv = document.createElement("iframe")
+MainDiv.classList.add("MainDiv")
+MainDiv.src = "https://lurkers.io/?host=51-222-105-196.lurkers.io&port=6775"
+MainDiv.style.height = "100%"
+MainDiv.style.width = "100%"
+MainDiv.style.top = "0px"
+MainDiv.style.backgroundColor = "rgba(125, 125, 125, 0.75)"
+MainDiv.style.position = "fixed"
+MainDiv.style.zIndex = "999"
+MainDiv.style.borderRadius = "15px"
 
-
-
-    Array.from(AllAds).forEach(Ad => {
-        Ad.remove()
-    })
-}
-
-RemoveAds()
+document.body.appendChild(MainDiv)
