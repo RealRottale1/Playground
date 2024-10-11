5dca976b2265270d9a724f00d7d32df1c17cd456
RealRottale1 <camarkham2@gmail.com>
2024-10-11 11:36:30 -0400

    e

--- SnakeButInjectable.js
+++ SnakeButInjectable.js
@@ -12,7 +12,7 @@ document.body.appendChild(MainDiv)
 const SnakeHead = document.createElement("div")
 SnakeHead.classList.add("SnakeHead")
 SnakeHead.style.backgroundColor = "green"
-MainDiv.style.zIndex = "9999"
+//MainDiv.style.zIndex = "9999"
 SnakeHead.style.height = "25px"
 SnakeHead.style.width = "25px"
 SnakeHead.style.position = "relative"
@@ -21,11 +21,13 @@ SnakeHead.style.left = "50%"
 MainDiv.appendChild(SnakeHead)
 
 // Variables And Stuff
-let SnakePosition = []
-const HeadPosition = [50, 50]
-const DirUp = [0, 0] // Direction, Up
 const MoveBy = 5
+const TickRate = 100
 
+let SnakePositions = [[50,50]]
+let SnakeParts = [SnakeHead]
+
+const DirUp = [0, 0] // Direction, Up
 
 function GetDirection(Key) {
     if (typeof(Key) != "string") {
@@ -45,8 +47,51 @@ function GetDirection(Key) {
     }
 }
 
+// Controls
 document.addEventListener("keydown", function(event) {
     console.log(event.key)
     const [Direction, Up] = GetDirection(event.key)
-    console.log(Direction+","+Up)
+    if (Direction) {
+        DirUp[0], DirUp[1] = Direction, Up
+    }
 })
+
+async function TickWait() {
+    return new Promise(Results => {
+        setTimeout(() => {
+            Results()
+        },TickRate)
+    })
+}
+
+function GrowSnake() {
+    const SnakeBody = document.createElement("div")
+    SnakeBody.classList.add("SnakeBody")
+    SnakeBody.style.backgroundColor = "green"
+    SnakeBody.style.height = "25px"
+    SnakeBody.style.width = "25px"
+    SnakeBody.style.position = "relative"
+    SnakeBody.style.top = "50%"
+    SnakeBody.style.left = "50%"
+    MainDiv.appendChild(SnakeBody)
+}
+
+function HandleSnakePart(UseSnakePosition, UseSnakePart) {
+
+}
+
+// Game Loop
+while (true) {
+    await TickWait()
+    console.log(SnakePositions[0])
+    if (DirUp[0]) {
+        HandleSnakePart(SnakePositions[0], SnakeParts[0])
+    }
+    const SnakeLength = SnakePositions.length
+    for (let i = 0; i < SnakeLength; i++) {
+        const UseSnakePosition = SnakePositions[i]
+        const UseSnakePart = SnakeParts[i]
+    }
+}
+
+//        SnakePositions[0][Up] += Direction*MoveBy
