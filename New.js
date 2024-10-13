3ff3d62053bec61bb505578326f753bce95e98d1
RealRottale1 <camarkham2@gmail.com>
2024-10-13 14:37:25 -0400

    Major progress SuperDiv function

--- New.js
+++ New.js
@@ -5,35 +5,46 @@ DragDiv.style.left = "0px"
 DragDiv.style.top = "0px"
 DragDiv.style.height = "100px"
 DragDiv.style.width = "125px"
+DragDiv.style.border = "solid red 35px"
 DragDiv.style.backgroundColor = "black"
 DragDiv.style.zIndex = "100"
 document.body.append(DragDiv)
 
 
 function SuperDiv(Div) {
-    const ResizeRange = 25
+    const ResizeRange = 35
     const GrabbedAt = [null, null]
+    const DivSize = [Div.style.width.replace("px",""), Div.style.height.replace("px","")]
+    const DivPos = [Div.style.left.replace("px",""), Div.style.top.replace("px","")]
 
     document.addEventListener("mousemove", function(event) {
         if (GrabbedAt[0] && GrabbedAt[1]) {
-            const NewGrabAtX = event.pageX-Number(String(Div.style.left).replace("px",""))
-            const NewGrabAtY = event.pageY-Number(String(Div.style.top).replace("px",""))
 
-            const UseHeight = Number(Div.style.height.replace("px",""))
-            const UseWidth = Number(Div.style.width.replace("px",""))
+            const NewGrabAtX = event.pageX-DivPos[0]
+            const NewGrabAtY = event.pageY-DivPos[1]
 
             const XMin = (GrabbedAt[0] <= ResizeRange)
-            const XMax = (GrabbedAt[0] >= UseWidth-ResizeRange)
+            const XMax = (GrabbedAt[0] >= DivSize[0]-ResizeRange)
             const YMin = (GrabbedAt[1] <= ResizeRange)
-            const YMax = (GrabbedAt[1] >= UseHeight-ResizeRange)
+            const YMax = (GrabbedAt[1] >= DivSize[1]-ResizeRange)
 
+
+            //console.log((DivSize[0]-NewGrabAtX)*-1)
             if ((XMin || XMax) && !YMin && !YMax) {
                 console.log("XRange")
-                Div.style.width = `${UseWidth+(NewGrabAtX-GrabbedAt[0])}px`
-                
+                const XChange = (NewGrabAtX-GrabbedAt[0])*(XMin ? 1 : -1)
+                Div.style.width = `${DivSize[0]-XChange}px`
+                if (XMin) {
+                    Div.style.left = `${DivPos[0]-(XChange*-1)}px`
+                }
+
             } else if ((YMin || YMax) && !XMin && !XMax) {
                 console.log("YRange")
-                Div.style.height = `${UseHeight+(NewGrabAtY-GrabbedAt[1])}px`
+                const YChange = (NewGrabAtY-GrabbedAt[1])*(YMin ? 1 : -1)
+                Div.style.height = `${DivSize[1]-YChange}px`
+                if (YMin) {
+                    Div.style.top = `${DivPos[1]-(YChange*-1)}px`
+                }
 
             } else {
                 console.log("DragRange")
@@ -57,11 +68,19 @@ function SuperDiv(Div) {
     Div.addEventListener("mouseup", function() {
         GrabbedAt[0] = null
         GrabbedAt[1] = null
+        DivSize[0] = Div.style.width.replace("px","")
+        DivSize[1] = Div.style.height.replace("px","")
+        DivPos[0] = Div.style.left.replace("px","")
+        DivPos[1] = Div.style.top.replace("px","")
     })
 
     Div.addEventListener("mouseleave", function() {
         GrabbedAt[0] = null
         GrabbedAt[1] = null
+        DivSize[0] = Div.style.width.replace("px","")
+        DivSize[1] = Div.style.height.replace("px","")
+        DivPos[0] = Div.style.left.replace("px","")
+        DivPos[1] = Div.style.top.replace("px","")
     })
 }
 
