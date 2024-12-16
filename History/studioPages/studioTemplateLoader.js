986253d94a7b990251a3500ca872600d0d617774
RealRottale1 <camarkham2@gmail.com>
2024-12-16 16:08:46 -0500

    Finished all studio stuff

--- History/studioPages/studioTemplateLoader.js
+++ History/studioPages/studioTemplateLoader.js
@@ -57,10 +57,73 @@ const templateData = {
     },
     '8': {
         name: 'Destroy the community',
-        image: '../images/climbTheTower.png',
+        image: '../images/destroyTheCommunity.png',
         description: 'In this game (a parody of one of my favorite games called Destroy the Neighborhood), you get tools to destroy the local neighborhood. Destroying stuff gives you money, which can be used to buy more tools and even power up disaster machines. There are many secrets hidden around the map waiting to be found.',
-        importance: 'This game is very important because it saw lots of innovation. Similarly to Pirate Ship Island (the game I made before this game), this game has lots of new, never-before-seen features like slippery tiles, speed tiles, hammers that break glass, placeable wood planks, gears that move platforms up and down, and temporary tiles.',
+        importance: "This game is important because this game made me play around with Roblox's physics, which allowed me to learn a lot of useful information, like how to make objects float.",
         playPage: 'https://www.roblox.com/games/7291473062/Destroy-the-Community',
+    },
+    '9': {
+        name: 'Puzzle box',
+        image: '../images/puzzleBox.png',
+        description: 'In this game you find yourself in a large hallway of boxes, each with different puzzles. Your main goal is to escape, but in order to escape, you must solve the puzzles in each box.',
+        importance: "This game is important because it helped to push the limits of my code. This was mainly done through the unique and challenging-to-code puzzles that often required me to think outside the box and look up forms for help.",
+        playPage: 'https://www.roblox.com/games/7610411255/Puzzle-box',
+    },
+    '10': {
+        name: 'team battle',
+        image: '../images/teamBattle.png',
+        description: 'In this game you join a team and battle the other team on a variety of different maps. Each map contains a multitude of exclusive traps.',
+        importance: 'During the process of making this game, I encountered immense trouble with the GUI score system and the guns that I had made. I would eventually stop production of the game because of these difficulties, but this game is very important because these difficulties helped me find out about the existence of clients. Up to this point I thought the server was the only component of the game (and that the client did not exist).',
+        playPage: 'https://www.roblox.com/games/7709530660/team-battle',
+    },
+    '11': {
+        name: 'The Local Grocery Store',
+        image: '../images/theLocalGroceryStore.png',
+        description: 'In this game you can be a customer, a worker, a cop, or a robber at the local grocery store. You can taste yummy food, go gambling, get lost in the play area, restock the shelves, clean spills, and arrest thieves.',
+        importance: "This game was monumental in pushing the boundaries of my code. This game was my first game where I used remote events (client-to-server or server-to-client communication). This game was also my first game where I used Roblox's collection service and is also the first time I used major for loops. On top of all of this, this was my first game to have 100% of the GUI made and coded by me.",
+        playPage: 'https://www.roblox.com/games/8496397459/The-Local-Grocery-Store',
+    },
+    '12': {
+        name: 'Build',
+        image: '../images/build.png',
+        description: 'In this game you can build anything you can imagine using a wide variety of build parts, colors, textures, and properties. What will you build?',
+        importance: "This game was my first game to dive deep into raycasting. This game not only pushed my use of raycasting to the limit, but it also pushed my use of client-server communication to the limit as well.",
+        playPage: 'https://www.roblox.com/games/8491744511/Untitled-Game',
+    },
+    '13': {
+        name: 'Housing Test',
+        image: '../images/housingTest.png',
+        description: 'In this game you must always watch your back. You can buy guns, bombs, and melee weapons to protect yourself and houses to hide in. You can also assemble a team or go solo and rob banks for loads of money.',
+        importance: "This game really helped push the boundaries not only for raycasting and GUIs but also for player-resistant systems (systems made extremely hard for a player to break). While I never finished this game, I have used multiple components from it, including the turret system and the health bar system.",
+        playPage: 'https://www.roblox.com/games/9204593253/Housing-Test',
+    },
+    '14': {
+        name: 'Protect the server',
+        image: '../images/protectTheServer.png',
+        description: 'In this game your main goal is to keep the server alive by completing wacky challenges. You can earn coins and gems, which can be used to buy gear and pets.',
+        importance: "This game is important because it introduced me to multi-server (same game, different server) communication and viewport rendering (having a 3D item render in a 2D frame). The game also made me construct my first-ever data storage system, which was not very good but would pave the way for better, more advanced ones later on.",
+        playPage: 'https://www.roblox.com/games/9763741821/Protect-the-server',
+    },
+    '15': {
+        name: 'SciTech Underground Facility',
+        image: '../images/scitechUndergoundFacility.png',
+        description: "In this game you work at an underground facility. Your job is to keep the facility's power core from overheating or undercooling among a multitude of smaller yet still important tasks. You can find items scattered across the facility that can be used to craft parts to repair machines and weapons to fend off against hostiles. With plenty of lab rooms to play around with, you are sure to clock out with an extra limb or two. You can also unlock a dimension room and travel across dimensions. Can you unravel what went on here before all of reality as we know it is destroyed?",
+        importance: "This game is important for a multitude of different reasons. The biggest one being that I spent real-life money on advertising this game (albeit not that much) and planned to keep advertising the game. The next reason why this game is important is because it redefined what I could do with code. This game has so many never-seen-before mechanics and concepts, like a custom inventory system, a dynamically fitting space suit, a crafting/purchasing system, a dynamic reset system, and so many more.",
+        playPage: 'https://www.roblox.com/games/14337062579/SciTech-Underground-Facility',
+    },
+    '16': {
+        name: 'Skibidi Toilet Roleplay',
+        image: '../images/skibidiToiletRoleplay.png',
+        description: 'In this game you can morph into your favorite characters from the skibidi universe and skibidi multiverse and explore the lore-accurate world.',
+        importance: "This game is important because it showed me that making video games is a viable career path (as proven by me making, in total, 2,700 USD off of this game). This game also made me a master at making models in Roblox Studio (professional level). This game taught me many things, like how to make custom playable characters, monetize my games, save and load player data in a safe and long-term format, deal with lots of pressure and an unending deadline, and how to ensure customer satisfaction.",
+        playPage: 'https://www.roblox.com/games/15029105106/NEW-TITAN-Skibidi-Toilet-Roleplay',
+    },
+    '17': {
+        name: 'Protect The Server',
+        image: '../images/protectTheServer2.png',
+        description: 'In this game your job is to protect the server by completing random challenges. You can earn coins and diamonds, which can be used to buy items, trails, pets, name themes, emotes, and even event rounds.',
+        importance: "This is by far the most complex video game I have ever made. This game has a fully dynamic 3D rendering shop, a daily reward system, over 39 different minigames, a plinko machine, exclusive VIP and premium benefits, multiple monetization methods, a game pass gifting system, a music system linked with the game's status, and a dynamic auto patching/updating data system that allows for easy data structure reform. This game was also my first game to utilize module scripts and is overall a very professional game.",
+        playPage: 'https://www.roblox.com/games/18475941513/Protect-The-Server',
     },
 };
 
@@ -72,6 +135,7 @@ if (!templateData[pageID]) {
 const gameTitle = document.getElementById('game_name');
 const secondGameTitle = document.getElementById('name_of_game');
 const gameImage = document.getElementById('gameplay_img');
+const aboutDiv = document.getElementById('about');
 const aboutDescription = document.getElementById('about_description');
 const aboutImportance = document.getElementById('importance_description');
 const playGameButton = document.getElementById('play_button');
@@ -80,6 +144,19 @@ gameTitle.textContent = templateData[pageID].name;
 secondGameTitle.textContent = templateData[pageID].name;
 gameImage.src = templateData[pageID].image;
 aboutDescription.textContent = templateData[pageID].description;
+if (templateData[pageID].name.length >= 25) {
+    aboutDiv.style.top = '150px';
+};
+
+const aboutDescriptionLength = aboutDescription.textContent.split(' ').length;
+console.log(aboutDescriptionLength);
+if (aboutDescriptionLength <= 40) {
+    aboutDescription.style.fontSize = '32px';
+} else if (aboutDescriptionLength >= 75) {
+    secondGameTitle.style.fontSize = '40px';
+    aboutDiv.style.top = '85px';
+    aboutDescription.style.fontSize = '22px'; 
+};
 aboutImportance.textContent = templateData[pageID].importance;
 
 playGameButton.addEventListener('click', function() {
