950942bf8caa5fd003e22710388a29ef79d45145
RealRottale1 <camarkham2@gmail.com>
2025-01-02 13:26:11 -0500

    Create caesarCipher.cpp

---
+++ CPPCreations/caesarCipher.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+
+
+std::string ceasafy(std::string plainText, int shiftBy) {
+    std::string unplainText;
+    const int plainTextLength = plainText.length();
+    for (int i = 0; i < plainTextLength; i++) {
+        int id = int(plainText[i]);
+        if ((id >= 65 && id <= 90) || (id >= 97 && id <= 122)) {
+            unplainText += ((id-65-shiftBy)%26)+65;
+            //(id<=90?(((id-65+shiftBy)%26)+65):((id-97+shiftBy)%26)+97);
+        } else {
+            unplainText += plainText[i];
+        }
+    }
+    return unplainText;
+}
+
+int main() {
+    std::string plainText = "aaabbbccc";
+    std::string unplainText = ceasafy(plainText, -3); 
+    std::cout << unplainText;
+    return 0;
+}
+
+//abcdefghijklmnopqrstuvwxyz
