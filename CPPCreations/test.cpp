b4e6a65e6788f465826726b481516b38a8e3b501
RealRottale1 <camarkham2@gmail.com>
2025-01-06 17:42:33 -0500

    Learning Pointers !

--- CPPCreations/test.cpp
+++ CPPCreations/test.cpp
@@ -2,25 +2,31 @@
 #include <string>
 
 int main() {
-    int* ptr1 = new int;
-    *ptr1 = 1;
+    std::string* pntr1 = new std::string;
+    *pntr1 = "Hello!";
 
-    std::cout << *ptr1 << std::endl;
+    std::cout << *pntr1 << std::endl;
 
-    int var1 = 2;
-    delete ptr1;
-    ptr1 = nullptr;
-    std::cout << "ptr1: " << ptr1 << std::endl;
-    ptr1 = &var1;
-    std::cout << ptr1 << std::endl;
+    std::string* pntr2 = new std::string;
+    *pntr2 = "Bello!";
 
-    std::cout << *ptr1 << std::endl;
-    std::cout << ptr1 << ',' << &var1 << std::endl;
+    std::cout << *pntr2 << std::endl;
 
-    *ptr1 = 3;
-    std::cout << *ptr1 << ',' << var1 << std::endl;
+    std::string GoodbyeMessage = "Goodbye!";
 
+    delete pntr2;
+    pntr2 = nullptr;
+    
+    pntr2 = &GoodbyeMessage;
 
-    std::cout << var1 << std::endl;
+    std::cout << *pntr2 << std::endl;
+
+    delete pntr1;
+    pntr1 = nullptr;
+
+    pntr1 = pntr2;
+
+    std::cout << pntr1 << "," << pntr2 << std::endl;
+
     return 0;
 }
