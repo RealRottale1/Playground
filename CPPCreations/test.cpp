6ef8513fcc4686dd2c06dbdf271c94c500bf0dba
RealRottale1 <camarkham2@gmail.com>
2025-01-11 09:38:20 -0500

    Box intersection code

--- CPPCreations/test.cpp
+++ CPPCreations/test.cpp
@@ -1,18 +1,17 @@
-/*
-    collision = Map -inf, 0, inf
-        chunk = Array containing yHeight * Arrays
-            Array = block id
-
-    background = same as collision map but if a collision block is present the background block is not rendered
-
-    rendering {
-        background
-        collision
-        player
+static bool intersects(std::array<float, 2> box1, float box1SizeX, float box1SizeY, std::array<float, 2> box2, float box2SizeX, float box2SizeY)
+{   
+    for (int i = 0; i < 1; i++)
+    {
+        const float boxSize = (i ? box1SizeY : box1SizeX);
+        const std::array<float, 2> pos1 = { box1[i] - boxSize / 2.0f, box1[i] + boxSize / 2.0f };
+        const std::array<float, 2> pos2 = { box2[i] - boxSize / 2.0f, box2[i] + boxSize / 2.0f };
+        const std::array<float, 2>* lowestPos = (pos1[i] >= pos2[i] ? &pos2 : &pos1);
+        const std::array<float, 2>* nextPos = (lowestPos == &pos1 ? &pos2 : &pos1);
+        const bool intersectsX = (((*nextPos)[i] >= (*lowestPos)[i]) && ((*nextPos)[i] <= (*lowestPos)[1]));
+        if (!intersectsX)
+        {
+            return false;
+        }
     }
-
-    detecting hits {
-        xPos from center of screen = xPos % xScreenSize
-        
-    }
-*/
+    return true;
+}
