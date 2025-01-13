152fa0399e151e647e3961e3061525a0e274cd09
RealRottale1 <camarkham2@gmail.com>
2025-01-13 07:10:04 -0500

    _

--- CPPCreations/test.cpp
+++ CPPCreations/test.cpp
@@ -1,17 +1,15 @@
-static bool intersects(std::array<float, 2> box1, float box1SizeX, float box1SizeY, std::array<float, 2> box2, float box2SizeX, float box2SizeY)
-{   
-    for (int i = 0; i < 1; i++)
-    {
-        const float boxSize = (i ? box1SizeY : box1SizeX);
-        const std::array<float, 2> pos1 = { box1[i] - boxSize / 2.0f, box1[i] + boxSize / 2.0f };
-        const std::array<float, 2> pos2 = { box2[i] - boxSize / 2.0f, box2[i] + boxSize / 2.0f };
-        const std::array<float, 2>* lowestPos = (pos1[i] >= pos2[i] ? &pos2 : &pos1);
-        const std::array<float, 2>* nextPos = (lowestPos == &pos1 ? &pos2 : &pos1);
-        const bool intersectsX = (((*nextPos)[i] >= (*lowestPos)[i]) && ((*nextPos)[i] <= (*lowestPos)[1]));
-        if (!intersectsX)
-        {
-            return false;
-        }
-    }
-    return true;
+#include <iostream>
+#include <array>
+
+int main()
+{
+    std::array<char, 3> array1 = {'a', 'b', 'c'};
+
+    char *charPointer = &array1[1];
+    std::array<char, 3> *arrayPointer = &array1;
+
+    std::cout << &charPointer << std::endl;
+    std::cout << &(*arrayPointer)[1] << std::endl;
+
+    return 0;
 }
