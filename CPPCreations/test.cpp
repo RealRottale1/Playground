5e5192aabe147f81a637e0813c0f50dcda85b7a5
RealRottale1 <camarkham2@gmail.com>
2025-01-15 12:58:37 -0500

    Update test.cpp

--- CPPCreations/test.cpp
+++ CPPCreations/test.cpp
@@ -2,30 +2,11 @@
 #include <array>
 #include <string>
 
-struct gameSettings
-{
-    static constexpr int refreshRate = 64;
-    static constexpr unsigned int screenX = 1200;
-    static const unsigned int screenY = 1000;
-};
-
-struct player
-{
-    double health = 100.0d;
-    std::array<float, 2> position = {0.0f, 0.0f};
-};
-
 int main()
 {
-    std::cout << "Refresh Rate: " << gameSettings::refreshRate << std::endl;
-    std::cout << "Screen Size X: " << gameSettings::screenX << std::endl;
-    std::cout << "Screen Size Y: " << gameSettings::screenY << std::endl;
+    int *number = new int;
+    *number = 256;
 
-    player rottale1;
-    player *pntr = &rottale1;
-
-    std::cout << "Player Health: " << (*pntr).health << std::endl;
-    std::cout << "Player Position: " << pntr->position[0] << ", " << pntr->position[1];
-
+    std::cout << number << ", " << &number << std::endl;
     return 0;
 }
