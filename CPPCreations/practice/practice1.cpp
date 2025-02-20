f9f05ef9993b6b67f45e77e738c7ff58a500ff84
RealRottale1 <camarkham2@gmail.com>
2025-02-20 11:46:22 -0500

    Smth

--- CPPCreations/practice/practice1.cpp
+++ CPPCreations/practice/practice1.cpp
@@ -1,54 +1,32 @@
 #include <iostream>
 #include <string>
 
-struct block {
-    int position[3] = {0, 0, 0};
-    std::string name;
-
-    block(int x, int y, int z, std::string name) {
-        this->position[0] = x;
-        this->position[1] = y;
-        this->position[2] = z;
-        this->name = name;
-    }
-    
-    std::string printPosition() {
-        return "X:" + std::to_string(this->position[0]) + ", Y:" + std::to_string(this->position[1]) + ", Z:" + std::to_string(this->position[2]);
-    }
-};
-
 int main() {
+    std::string text = "0";
 
-    block dirt(5, 15, 10, "dirt");
-    std::cout << dirt.printPosition();
+    std::string *heapPntr1 = new std::string; // Heap Pointer
+    *heapPntr1 = text;
+    *heapPntr1 = "1";
 
-    std::cout << std::endl;
+    std::string *stackPntr1 = &text; // Stack Pointer
+    *stackPntr1 = "2";
 
-    block *pntr = &dirt; // Stack Pointer
-    std::cout << "Memory location of dirt stored: " << &dirt << std::endl;
-    std::cout << "Memory location of dirt stored as the value of a pointer: " << pntr << std::endl; // Use pntr-> to access values not *pntr. This is because the pntr points to an instance and not a value and thus needs the -> operator to access any values (pntr holds a refrence not a value)
-    std::cout << "Memory location of a pointer that points to the memory location of dirt" << &pntr << std::endl;
-    
-    /////////////////////Seperate Testing/////////////////////
-    int *numPntr = new int; // Heap Pointer
-    *numPntr = 9;
+    std::string *stackPntr2 = stackPntr1; // Stack Pointer
+    *stackPntr2 = "3";
 
-    int *stackPntr = numPntr; // Stack Pointer
+    std::string *stackPntr5 = stackPntr2; // Stack Pointer
+    *stackPntr5 = "5";
 
+    std::cout << &text << " , " << stackPntr1 << " , " << stackPntr2 << " , " << stackPntr5 << std::endl;
 
-    std::cout << "Heap pntr value: " << *numPntr << std::endl;
-    std::cout << "Stack pntr value: " << *stackPntr << std::endl;
-    std::cout << "Heap pntr stores memory location to int value: " << numPntr << std::endl;
-    std::cout << "Stack pntr stores memory location to int value: " << stackPntr << std::endl;
-    std::cout << "Memory location of heap pntr: " << &numPntr << std::endl;
-    std::cout << "Memory location of stack pntr: " << &stackPntr << std::endl;
+    std::string **stackPntr3 = &stackPntr2;
+    **stackPntr3 = "4";
 
-    stackPntr = nullptr;
-    delete numPntr; // You must manually delete heap data
-    numPntr = nullptr;
-
-    std::cout << stackPntr << std::endl;
-    std::cout << numPntr << std::endl;
+    std::cout << "text: " << text << std::endl;
+    std::cout << "*heapPntr1: " << *heapPntr1 << std::endl;
+    std::cout << "*stackPntr1: " << *stackPntr1 << std::endl;
+    std::cout << "*stackPntr2: " << *stackPntr2 << std::endl;
+    std::cout << "**stackPntr3: " << **stackPntr3 << std::endl;
 
     return 0;
 }
