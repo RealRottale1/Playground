7042bf9037391d8e10e23494a39e3d9c94a31ce2
RealRottale1 <camarkham2@gmail.com>
2025-03-29 11:57:43 -0400

    Doing some work

--- CPPCreations/fourInARow.cpp
+++ CPPCreations/fourInARow.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <map>
+#include <vector>
 
 std::array<std::array<char, 7>, 6> makeGameBoard() {
     std::array<std::array<char, 7>, 6> gameBoard;
@@ -55,6 +57,90 @@ bool isValidPlayerMove(std::array<std::array<char, 7>, 6> &gameBoard, std::strin
     return true;
 }
 
+std::string getPositionName(int row, int column) {
+    std::string positionName = std::to_string(row) + char(column + 65);
+    return positionName;
+}
+
+void botMove(std::array<std::array<char, 7>, 6> &gameBoard) {
+    std::map<std::string, int> allMoves = {};
+
+    auto assignPointsToPosition = [&allMoves](int row, int column, int points) {
+        std::string sidePositionName = getPositionName(row, column);
+        if (allMoves.find(sidePositionName) == allMoves.end()) {
+            allMoves.insert({sidePositionName, points});
+        } else {
+            allMoves[sidePositionName] += 1;
+        }
+    };
+
+    for (int row = 0; row < 6; row++) {
+        for (int column = 0; column < 7; column++) {
+            char currentPieceType = gameBoard[row][column];
+            if (currentPieceType != '_') {
+                
+                // Handles sides
+                bool leftInGrid = (column - 1 != -1);
+                bool rightInGrid = (column + 1 != 8);
+                
+                if (!leftInGrid || !rightInGrid) { // No pair
+                    int useDif = (leftInGrid ? -1 : 1);
+                    assignPointsToPosition(row, column + useDif, 1);
+                } else {
+                    char leftPiece = gameBoard[row][column - 1];
+                    char rightPiece = gameBoard[row][column + 1];
+                    if (leftPiece == rightPiece && leftPiece == currentPieceType) { // 3 pair connected
+                        bool farLeftInGrid = (column - 2 > -1);
+                        bool farRightInGrid = (column + 2 < 8);
+                        
+                        char farLeftPiece = (farLeftInGrid ? gameBoard[row][column - 2] : 'N');
+                        char farRightPiece = (farRightInGrid ? gameBoard[row][column + 2] : 'N');
+
+                        if (farLeftPiece == farRightPiece && farLeftPiece == '_') { // 3 pair connected with 2 openings
+                            assignPointsToPosition(row, column - 2, 100 * (currentPieceType == 'C' ? 10 : 1));
+                            assignPointsToPosition(row, column + 2, 100 * (currentPieceType == 'C' ? 10 : 1));
+                        } else if (farLeftPiece == '_' || farRightPiece == '_') { // 3 pair connected with 1 opening
+                            int useDif = (farLeftInGrid ? -2 : 2);
+                            assignPointsToPosition(row, column + useDif, 100 * (currentPieceType == 'C' ? 10 : 1));
+                        }
+
+                    } else if (leftPiece == currentPieceType || rightPiece == currentPieceType) { // 2 pair connected
+                        assignPointsToPosition(row, column + (leftPiece == currentPieceType ? -1 : 1), 10);
+                    } else if (leftPiece == '_' || rightPiece == '_') {
+                        if (leftPiece == rightPiece) { // Both sides empty
+                            assignPointsToPosition(row, column - 1, 1);
+                            assignPointsToPosition(row, column + 1, 1);
+                        } else { // 1 side empty
+                            int useDif = (leftPiece == '_' ? -1 : 1);
+                            assignPointsToPosition(row, column + useDif, 1);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    for (auto &pair : allMoves) {
+        std::cout << pair.first << " , " << pair.second << std::endl;
+    }
+}
+
+/*
+                // Handles sides
+                for (int dif = 0; dif < 2; dif++) {
+                    int sideColumn = column + (dif ? 1 : -1);
+                    if (sideColumn > -1 && sideColumn < 8) {
+                        std::string sidePositionName = getPositionName(row, sideColumn);
+                        if (allMoves.find(sidePositionName) == allMoves.end()) {
+                            allMoves.insert({sidePositionName, 1});
+                        } else {
+                            allMoves[sidePositionName] += 1;
+                        }
+                    }
+                }
+*/
+
 int main() {
     std::array<std::array<char, 7>, 6> gameBoard = makeGameBoard();
 
@@ -67,6 +153,8 @@ int main() {
             std::getline(std::cin, playerMove);
             validPlayerMove = isValidPlayerMove(gameBoard, playerMove);
         } while (!validPlayerMove);
+        // Check if player won
+        botMove(gameBoard);
     }
     return 0;
 }
