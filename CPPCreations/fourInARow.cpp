71a0466486f422bebc8e779182e2af220807d549
RealRottale1 <camarkham2@gmail.com>
2025-03-29 17:13:53 -0400

    Done for the day

--- CPPCreations/fourInARow.cpp
+++ CPPCreations/fourInARow.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <cmath>
 
 std::array<std::array<char, 7>, 6> makeGameBoard() {
     std::array<std::array<char, 7>, 6> gameBoard;
@@ -70,53 +71,113 @@ void botMove(std::array<std::array<char, 7>, 6> &gameBoard) {
         if (allMoves.find(sidePositionName) == allMoves.end()) {
             allMoves.insert({sidePositionName, points});
         } else {
-            allMoves[sidePositionName] += 1;
+            allMoves[sidePositionName] += points;
         }
+    };
+
+    auto getPiece = [&gameBoard](int row, int column) {
+        char piece = gameBoard[row][column];
+        if (row !=5) {
+            char supportingPiece = gameBoard[row + 1][column];
+            if (supportingPiece == '_') {
+                return 'N';
+            }
+        }
+        return piece;
     };
 
     for (int row = 0; row < 6; row++) {
         for (int column = 0; column < 7; column++) {
-            char currentPieceType = gameBoard[row][column];
-            if (currentPieceType != '_') {
-                
-                // Handles sides
-                bool leftInGrid = (column - 1 != -1);
-                bool rightInGrid = (column + 1 != 8);
-                
-                if (!leftInGrid || !rightInGrid) { // No pair
-                    int useDif = (leftInGrid ? -1 : 1);
-                    assignPointsToPosition(row, column + useDif, 1);
-                } else {
-                    char leftPiece = gameBoard[row][column - 1];
-                    char rightPiece = gameBoard[row][column + 1];
-                    if (leftPiece == rightPiece && leftPiece == currentPieceType) { // 3 pair connected
-                        bool farLeftInGrid = (column - 2 > -1);
-                        bool farRightInGrid = (column + 2 < 8);
-                        
-                        char farLeftPiece = (farLeftInGrid ? gameBoard[row][column - 2] : 'N');
-                        char farRightPiece = (farRightInGrid ? gameBoard[row][column + 2] : 'N');
-
-                        if (farLeftPiece == farRightPiece && farLeftPiece == '_') { // 3 pair connected with 2 openings
-                            assignPointsToPosition(row, column - 2, 100 * (currentPieceType == 'C' ? 10 : 1));
-                            assignPointsToPosition(row, column + 2, 100 * (currentPieceType == 'C' ? 10 : 1));
-                        } else if (farLeftPiece == '_' || farRightPiece == '_') { // 3 pair connected with 1 opening
-                            int useDif = (farLeftInGrid ? -2 : 2);
-                            assignPointsToPosition(row, column + useDif, 100 * (currentPieceType == 'C' ? 10 : 1));
-                        }
-
-                    } else if (leftPiece == currentPieceType || rightPiece == currentPieceType) { // 2 pair connected
-                        assignPointsToPosition(row, column + (leftPiece == currentPieceType ? -1 : 1), 10);
-                    } else if (leftPiece == '_' || rightPiece == '_') {
-                        if (leftPiece == rightPiece) { // Both sides empty
-                            assignPointsToPosition(row, column - 1, 1);
-                            assignPointsToPosition(row, column + 1, 1);
-                        } else { // 1 side empty
-                            int useDif = (leftPiece == '_' ? -1 : 1);
+            char currentPieceType = getPiece(row, column);
+            if (currentPieceType != 'N') {
+                if (currentPieceType != '_') {
+                    bool leftInGrid = (column - 1 != -1);
+                    bool rightInGrid = (column + 1 != 8);
+    
+                    if (!leftInGrid || !rightInGrid) { // No pair
+                        int useDif = (leftInGrid ? -1 : 1);
+                        if (getPiece(row, column + useDif) == '_') {
                             assignPointsToPosition(row, column + useDif, 1);
+                        }
+                    } else {
+                        char leftPiece = getPiece(row, column - 1);
+                        char rightPiece = getPiece(row, column + 1);
+                        if (leftPiece == rightPiece && leftPiece == currentPieceType) { // 3 pair connected
+                            bool farLeftInGrid = (column - 2 > -1);
+                            bool farRightInGrid = (column + 2 < 8);
+                            
+                            char farLeftPiece = (farLeftInGrid ? getPiece(row, column - 2) : 'N');
+                            char farRightPiece = (farRightInGrid ? getPiece(row, column + 2) : 'N');
+    
+                            if (farLeftPiece == farRightPiece && farLeftPiece == '_') { // 3 pair connected with 2 openings
+                                assignPointsToPosition(row, column - 2, 100 * (currentPieceType == 'C' ? 10 : 1) - 10);
+                                assignPointsToPosition(row, column + 2, 100 * (currentPieceType == 'C' ? 10 : 1)  - 10);
+                            } else if (farLeftPiece == '_' || farRightPiece == '_') { // 3 pair connected with 1 opening
+                                int useDif = (farLeftInGrid ? -2 : 2);
+                                assignPointsToPosition(row, column + useDif, 100 * (currentPieceType == 'C' ? 10 : 1)  - 10);
+                            }
+    
+                        } else if (leftPiece == currentPieceType || rightPiece == currentPieceType) { // 2 pair connected
+                            assignPointsToPosition(row, column + (leftPiece == currentPieceType ? 1 : -1), 10);
+                        } else if (leftPiece == '_' || rightPiece == '_') {
+                            if (leftPiece == rightPiece) { // Both sides empty
+                                assignPointsToPosition(row, column - 1, 1);
+                                assignPointsToPosition(row, column + 1, 1);
+                            } else { // 1 side empty
+                                int useDif = (leftPiece == '_' ? -1 : 1);
+                                assignPointsToPosition(row, column + useDif, 1);
+                            }
+                        }
+                    }
+    
+                    if (column < 4) {
+                        char endPiece = getPiece(row, column + 3);
+                        if (endPiece == currentPieceType) {
+                            char secondPiece = getPiece(row, column + 1);
+                            char thirdPiece = getPiece(row, column + 2);
+                            if (secondPiece == thirdPiece && secondPiece == '_') {
+                                assignPointsToPosition(row, column + 1, 10);
+                                assignPointsToPosition(row, column + 2, 10);
+                            } else if (secondPiece == '_' || thirdPiece == '_') {
+                                int useDif = (secondPiece == '_' ? 1 : 2);
+                                assignPointsToPosition(row, column + useDif, 10);
+                            }
+                        }
+                    }
+                } else {
+                    bool leftInGrid = (column - 1 != -1);
+                    bool rightInGrid = (column + 1 != 8);
+    
+                    if (leftInGrid && rightInGrid) {
+                        char leftPiece = getPiece(row, column - 1);
+                        char rightPiece = getPiece(row, column + 1);
+    
+                        if (leftPiece == rightPiece && leftPiece != '_') {
+                            assignPointsToPosition(row, column, 9);
                         }
                     }
                 }
+            }
+        }
+    }
 
+    for (int column = 0; column < 7; column++) {
+        int sameTypeCounter = 0;
+        char previousPieceType = 'N';
+        for (int row = 5; row >= 0; row--) {
+            char currentPieceType = gameBoard[row][column];
+            if (currentPieceType != '_') {
+                if (previousPieceType == 'N' || previousPieceType == currentPieceType) {
+                    sameTypeCounter += 1;
+                } else {
+                    sameTypeCounter = 0;
+                    previousPieceType = currentPieceType;
+                }
+            } else {
+                if (sameTypeCounter != 0) {
+                    assignPointsToPosition(row, column, std::pow(10, -1+sameTypeCounter) * ((currentPieceType == 'C' && sameTypeCounter == 3)  ? 10 : 1));
+                }
+                break;
             }
         }
     }
