6ed5ed36a8066bd306e95d5453a30fea8ace00b4
RealRottale1 <camarkham2@gmail.com>
2025-06-12 15:36:52 -0400

    More practice + Word Ladder

--- SwiftCreations/tests/test.swift
+++ SwiftCreations/tests/test.swift
@@ -59,7 +59,80 @@ func wordFrequency(_ s: String) -> [String: Int] {
     return frequency;
 }
 
-let results: [String: Int] = wordFrequency("Hello hello hello, ,hello bob bob dow");
-for (key, value) in results {
-    print("Key: \(key), Value: \(value)");
+func mostFrequentLetter(_ s: String) -> Character? {
+    let s: String = s.lowercased();
+    var f: [Int: [Character]] = [:];
+    var d: [Character: Int] = [:];
+    for c in s {
+        if c.isLetter {
+            d[c] = (d[c] ?? 0) + 1;
+            f[d[c]!, default: []].append(c);
+        }
+    }
+    if f.count == 0 {
+        return nil;
+    }
+    let m: Int = f.keys.max()!;
+    return f[m]!.first!;
 }
+
+func longestWord(_ s: String) -> String? {
+    if s.isEmpty {return nil};
+    let s: String = s.filter({$0.isLetter || $0==" "});
+    let a: [String.SubSequence] = s.split(separator: " ");
+    if let m = a.max(by: {$0.count < $1.count}) {
+        return String(m);
+    } else {
+        return nil;
+    }
+}
+
+func countVowelHeavyWords(_ s: String) -> Int? {
+    if s.isEmpty {return nil};
+    let s: String = s.lowercased().filter({$0.isLetter || $0==" "});
+    let a: [String.SubSequence] = s.split(separator: " ");
+    if a.count == 0 {return nil};
+    var t: Int = 0;
+    for w in a {
+        var vowels: Int = 0;
+        var consonants: Int = 0;
+        for c in w {
+            if ["a", "e", "i", "o", "u"].contains(c) {
+                vowels += 1;
+            } else {
+                consonants += 1;
+            }
+        }
+        if vowels > consonants {
+            t += 1;
+        }
+    }
+    return t;
+}
+
+func groupAnagrams(_ words: [String]) -> [[String]]? {
+    if words.isEmpty {return nil};
+    var holder: [[String]] = [];
+    var words: [String] = words.map({$0.lowercased()});
+    for i in stride(from: words.count-1, through: 0, by: -1) {
+        if i >= words.count {
+            continue;
+        }
+        for j in stride(from: words.count-1, through: 0, by: -1) {
+            if i == j {
+                continue;
+            }
+            if words[i] == String(words[j].reversed()) {
+                holder.append([words[i], words[j]]);
+                let check: Bool = i > j;
+                words.remove(at: check ? i : j);
+                words.remove(at: check ? j : i);
+                break;
+            }
+        }
+    }
+    return holder;
+}
+
+let input = ["bat", "tab", "tap", "pat", "eat", "tea", "tan", "nat"]; // 3 anagram pairs
+let output = groupAnagrams(input);
---
+++ SwiftCreations/wordLadder.swift
@@ -0,0 +1,71 @@
+let allWords: [String] = ["aah","aba","abs","ace","ach","act","add","ado","adz","aft","aga","age","ago","aha","ahi","aid","ail","aim","ain","air","ait","ala","alb","ale","all","alp","alt","amp","amu","ana","and","ane","ani","ant","any","ape","app","apt","arb","arc","are","arf","ark","arm","art","ash","ask","asp","ass","ate","auk","ave","avo","awe","awl","awn","axe","aye","azo","baa","bad","bag","bah","ban","bap","bar","bat","bay","bed","bee","beg","bel","ben","bet","bey","bib","bid","big","bin","bio","bis","bit","biz","boa","bob","bod","bog","boo","bop","bot","bow","box","boy","bra","bro","bub","bud","bug","bum","bun","bur","bus","but","buy","bye","cab","cad","cal","cam","can","cap","car","cat","caw","cay","cee","cel","cep","chi","cig","cob","cod","cog","col","con","coo","cop","cor","cos","cot","cow","cox","coy","coz","cru","cry","cub","cud","cue","cum","cup","cur","cut","cwm","dab","dad","dag","dah","dak","dal","dam","dan","dap","daw","day","deb","dee","def","del","den","dew","dex","dey","dib","did","die","dig","dim","din","dip","dis","dit","div","doc","doe","dof","dog","doh","don","dop","dor","dot","dry","dub","dud","due","dug","duh","dun","duo","dux","dye","dzo","ear","eat","ebb","ecu","edh","eek","eel","eff","eft","egg","ego","eke","eld","elf","elk","ell","elm","emo","emu","end","eon","era","ere","erf","erg","erk","err","ess","eta","eth","eve","ewe","eye","fab","fad","fag","fah","fan","far","fat","fax","fay","fed","fee","feh","fen","feu","few","fey","fez","fib","fid","fie","fig","fin","fir","fit","fix","flu","fly","fob","foe","fog","fop","for","fou","fox","foy","fro","fry","fug","fun","fur","gab","gad","gag","gal","gam","gap","gar","gas","gat","gay","gee","gel","gem","geo","get","gey","gib","gid","gie","gig","gin","gip","git","gnu","goa","gob","god","goo","got","goy","gub","gul","gum","gun","gur","gut","guv","guy","gym","gyp","had","hae","hag","hah","haj","ham","hao","hap","has","hat","haw","hay","hem","hen","hep","her","hew","hex","hey","hic","hid","hie","him","hin","hip","his","hit","hob","hod","hoe","hog","hom","hon","hop","hot","how","hoy","hub","hue","hug","huh","hum","hut","ice","ich","ick","icy","ide","iff","igg","ilk","ill","imp","ink","inn","ion","ire","irk","ism","its","ivy","iwi","jab","jag","jam","jar","jaw","jay","jee","jet","jew","jib","jig","job","joe","jog","jol","jot","joy","jug","jun","jus","jut","kab","kai","kay","kea","ked","kef","keg","ken","kep","key","khi","kid","kif","kin","kip","kir","kit","koa","kob","koi","kop","kor","kyu","lab","lac","lad","lag","lah","lam","lap","lat","law","lax","lay","lea","led","lee","leg","lei","lek","les","let","leu","lev","lex","ley","lez","lib","lid","lie","lig","lip","lis","lit","lob","log","loo","lop","lor","lot","low","lox","lud","lug","lum","lur","lux","lye","mac","mad","mag","mam","man","map","mar","mas","mat","maw","max","may","med","meg","mem","men","met","mew","mho","mic","mid","mil","mim","mir","mix","moa","mob","moc","mod","mog","moi","mol","mom","mon","moo","mop","mot","mow","mud","mug","mum","mun","mux"];
+var allConnections: [String: [String]] = [:];
+
+func offBy(fWord: [Character], lWord: [Character]) -> Int {
+    var mismatches: Int = 0;
+    for i in 0..<3 {
+        if fWord[i] != lWord[i] {
+            mismatches += 1;
+        }
+    }
+    return mismatches;
+}
+
+for connectWord in allWords {
+    let fWord: [Character] = Array(connectWord);
+    for branchWord in allWords {
+        if connectWord == branchWord {
+            continue;
+        }
+        let lWord: [Character] = Array(branchWord);
+        if offBy(fWord: fWord, lWord: lWord) == 1 {
+            allConnections[connectWord, default: []].append(branchWord);
+        }
+    }
+}
+
+func getShortestPath(sWord: String, eWord: String) -> [String] {
+    func getUnusedConnections(word: String, usedConnections: Set<String>) -> [String] {
+        var unusedConnections: [String] = [];
+        for connection in allConnections[word]! {
+            if !usedConnections.contains(connection) {
+                unusedConnections.append(connection);
+            }
+        }
+        return unusedConnections;
+    }
+
+    var usedConnections: Set<String> = [sWord];
+    var nextPathWays: [[String]] = [];
+    var pathWays: [[String]] = [];
+
+    let initialConnections: [String] = getUnusedConnections(word: sWord, usedConnections: usedConnections);
+    for connection in initialConnections {
+        pathWays.append([sWord, connection]);
+    }
+
+    var iterations: Int = 0;
+    while true {
+        for path in pathWays {
+            let pathLen: Int = path.count;
+            let newConnections: [String] = getUnusedConnections(word: path[pathLen-1], usedConnections: usedConnections);
+            for newConnection in newConnections {
+                usedConnections.insert(newConnection);
+                nextPathWays.append(path + [newConnection]);
+
+                if newConnection == eWord {
+                    return nextPathWays[nextPathWays.count-1];
+                }
+            }
+        }
+        if nextPathWays.isEmpty {
+            return ["Not possible"];
+        }
+        pathWays = nextPathWays;
+        nextPathWays.removeAll();
+        iterations += 1;
+    }
+}
+
+let results: [String] = getShortestPath(sWord: "cat", eWord: "fin");
+print(results);
