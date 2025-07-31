3f095c78e1b0a4b37d3277b5846d604413f1747c
RealRottale1 <camarkham2@gmail.com>
2025-07-31 13:58:02 -0400

    _

--- JavaCreations/Scrap_Yard/fallingPhysics.java
+++ JavaCreations/Scrap_Yard/fallingPhysics.java
@@ -1,35 +1,37 @@
 import java.util.*;
 
-public class Shape {
+class Shape {
     Integer y; // y position
     Integer x; // x position
     boolean moved;
 
     Integer yDir = 0; // y dir of movement
     Integer xDir = 0; // x dir of movement
-    Integer momentum = 0; // # times they move without slope
-    Integer momentumGain = 0; // Amount of momentum gained per slope
-    Integer fallGain = 0; // Amount of momentum gained per fall
+    Integer momentum = 0;
 
-    public Shape(Integer y, Integer x, Integer s, Integer f) {
+    public Shape(Integer y, Integer x) {
         this.y = y;
         this.x = x;
         this.moved = false;
-        this.momentumGain = s;
-        this.fallGain = f;
     }
 }
-
-public class
 
 public class Main {
     public static void display(char[][] board) {
         for (Integer y = 0; y < board.length; y++) {
             for (Integer x = 0; x < board[y].length; x++) {
-                System.out.println(board[y][x]);
+                System.out.print(board[y][x]);
             }
             System.out.println();
         }
+    }
+
+    public static char[][] cloneBoard(char[][] board) {
+        char[][] clone = new char[board.length][];
+        for (int i = 0; i < board.length; i++) {
+            clone[i] = Arrays.copyOf(board[i], board[i].length);
+        }
+        return clone;
     }
 
     public static void main(String[] args) {
@@ -43,66 +45,110 @@ public class Main {
             {'■','◣',' ','◢','■','■','■','■','■','■','■','■','■','■','■','■','■','■','■','■'},
             {'■','■','■','■','■','■','■','■','■','■','■','■','■','■','■','■','■','■','■','■'},
         };
+        Integer boardY = board.length;
+        Integer boardX = board[0].length;
 
-        HashMap<Integer, HashMap<Integer, Shape>> nextObjects = new HashMap<Integer, HashMap<Integer, Shape>>();
-        HashMap<Integer, HashMap<Integer, Shape>> objects = new HashMap<Integer, HashMap<Integer, Shape>>();
+        Set<Shape> objects = new HashSet<Shape>();
 
-        for (Integer y = 0; y < board.length; y++) {
-            for (Integer x = 0; x < board[y].length; x++) {
+        for (Integer y = 0; y < boardY; y++) {
+            for (Integer x = 0; x < boardX; x++) {
                 if (board[y][x] == '○') {
-                    Shape ball = new Shape(y, x, 6, 2);
-                    objects.computeIfAbsent(y, t -> new HashMap<Integer, Shape>()).put(x, ball);
+                    Shape ball = new Shape(y, x);
+                    objects.add(ball);
                 }
             }
         }
 
         do {
-            List<Integer> keys = new ArrayList<Integer>(objects.keySet());
-            keys.sort(Comparator.reverseOrder());
-            for (Integer y : keys) {
-                for (HashMap.Entry<Integer, Shape> entry : objects[y].entrySet()) {
-                    Integer x = entry.getKey();
-                    Shape object = entry.getValue();
-
-                    // If falling yDir = -1
-                    if (y+1 < 7 && board[y+1][x] == ' ') {
-                        object.yDir = -1;
-                        object.momentum += object.fallGain;
+            char[][] clonedBoard = cloneBoard(board);
+            do {
+                for (Shape object : objects) {
+                    // Checks if in air
+                    boolean inAir = false;
+                    if (object.y+1 < board.length && board[object.y+1][object.x] == ' ') {
+                        object.yDir = 1;
+                        object.momentum += 3;
+                        inAir = true;
                     }
 
-                    if (momentum == 0) {
-                        nextObjects.computeIfAbsent(object.y, t -> new HashMap<Integer, Shape>()).put(object.x, object);
-
-                    };
+                    if (object.momentum == 0) {
+                        continue;
+                    }
 
                     Integer nextY = object.y + object.yDir;
                     Integer nextX = object.x + object.xDir;
-                    if (nextX < 0 || nextX > 19) {
-                        // Out of bounds
-                        object.xDir = 0;
-                        nextX = object.x;
-                    } else {
 
+                    // Checks if y in range
+                    if (nextY >= boardY) {
+                        nextY = object.y;
                     }
+
+                    // Checks if x in range
+                    if (nextX < 0 || nextX > boardX) {
+                        nextX = object.x;
+                        object.xDir = 0;
+                    }
+                    System.out.println(nextY + "," + nextX);
                     char charInPos = board[nextY][nextX];
-                    if (charInPos == '◢' || charInPos == '■' || charInPos == '◣') {
-                            // Map in way
-                            object.momentum = 0;
-                            object.yDir = 0;
+                    if (charInPos == '■') {
+                        object.momentum -= 1;
+                        object.yDir = 0;
+                        if (object.xDir != 0 && object.yDir == 0) {
                             object.xDir = 0;
-                            continue;
-                        } else if (charInPos == ' ') {
-                            object.momentum -= 1;
-                            // Nothing in way
-                        } else {
-                            // Object in way
+                            object.momentum = 0;
                         }
+                    } else if (charInPos == '◢') {
+                        if (object.xDir == 0 || object.xDir == 1) {
+                            if (object.x-1 > -1) {
+                                object.xDir = -1;
+                                if (object.xDir == 0) {
+                                    object.momentum += 3;
+                                } else {
+                                    object.momentum -= 1;
+                                }
+                            }
+                        } else {
+                            object.xDir = 1;
+                            object.momentum -= 1;
+                        }
+                    } else if (charInPos == '◣') {
+                        if (object.xDir == 0 || object.xDir == -1) {
+                            if (object.x+1 < boardX) {
+                                object.xDir = 1;
+                                if (object.xDir == 0) {
+                                    object.momentum += 3;
+                                } else {
+                                    object.momentum -= 1;
+                                }
+                            }
+                        } else {
+                            object.xDir = -1;
+                            object.momentum -= 1;
+                        }
+                    } else if (charInPos == ' ') {
+                        if (!inAir) {
+                            object.momentum -= 1;
+                        }
+                        clonedBoard[object.y][object.x] = ' ';
+                        object.y = nextY;
+                        object.x = nextX;
+                        clonedBoard[object.y][object.x] = '○';
+                    } else {
+                        
                     }
+
+
                 }
-            }
+                break;
+            } while (true);
+            board = clonedBoard;
             display(board);
-            Thread.sleep(1000);
-        } while (true)
+            try {
+                Thread.sleep(1000); // waits 1000 milliseconds = 1 second
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (true);
     }
 }
 
