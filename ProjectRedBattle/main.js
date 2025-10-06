9c41af91d58927d99a06230b5e73518932748921
RealRottale1 <camarkham2@gmail.com>
2025-10-06 07:46:50 -0400

    Working on major logical revamp

--- ProjectRedBattle/main.js
+++ ProjectRedBattle/main.js
@@ -124,22 +124,6 @@ class BM {
     static map = [[]];
     static tiles = ["Grass", "Stone", "Shallow Water", "Deep Water", "Sand", "Lava"];
 
-    static renderDebugTile(x, y, i) {
-        const baseTileSize = WP.windowWidth / BM.maxColumns;
-        const tileSize = baseTileSize * BM.zoom;
-        const size = Math.ceil(tileSize);
-
-        const halfWidth = WP.windowWidth / 2;
-        const halfHeight = WP.windowHeight / 2;
-
-        const screenX = Math.floor((x - BM.mouseX) * tileSize + halfWidth);
-        const screenY = Math.floor((y - BM.mouseY) * tileSize + halfHeight);
-
-        ctx.fillStyle = "purple";
-        ctx.font = "25px serif";
-        ctx.fillText(i, screenX, screenY);
-    }
-
     static render() {
         const baseTileSize = WP.windowWidth / BM.maxColumns;
         const tileSize = baseTileSize * BM.zoom;
@@ -210,106 +194,53 @@ class GUI {
     }
 }
 
-/* AStar Path Finding */
-class AStar {
-    constructor(start, goal, tileProperties) {
-        this.start = start;
-        this.goal = goal;
-
-        this.open = [{position: start, g: 0, f: AStar.heuristic(start, goal), parent: null}];
-        this.closed = new Set();
-        this.finished = false;
-        this.path = null;
-
-        this.tileWeights = {};
-        for (const [key, data] of Object.entries(tileProperties)) {
-            this.tileWeights[key] = data.r;
-        }
-    }
-
-    static heuristic(a, b) {
-        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
-    }
-
-    static neighbors(currentPosition) {
-        const x = currentPosition[0];
-        const y = currentPosition[1];
-        const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-        const results = [];
-        for (const [dx, dy] of dirs) {
-            const nx = x + dx;
-            const ny = y + dy;
-            if (nx >= 0 && ny >= 0 && nx < BM.maxColumns && ny < BM.maxRows) {
-                results.push([nx, ny]);
-            }
-        }
-        return results;
-    }
-
-    static reconstruct(node) {
-        const path = [];
-        let current = node;
-        while (current) {
-            path.push(current.position);
-            current = current.parent;
-        }
-        return path.reverse();
-    }
-
-    static step(maxSteps, instance) {
-        for (let i = 0; i < maxSteps && instance.open.length > 0; i++) {
-            instance.open.sort((a, b) => a.f - b.f);
-            const current = instance.open.shift();
-            const key = current.position[0]+","+current.position[1];
-            if (instance.closed.has(key)) {continue};
-            instance.closed.add(key);
-            if (current.position[0] == instance.goal[0] && current.position[1] == instance.goal[1]) {
-                instance.finished = true;
-                instance.path = AStar.reconstruct(current);
-                return;
-            }
-            for (const neighbor of AStar.neighbors(current.position)) {
-                const [nx, ny] = neighbor;
-                const tileName = BM.map[ny][nx];
-                if (tileName == "stone") {continue};
-                const g = current.g + instance.tileWeights[tileName];
-                const h = AStar.heuristic(neighbor, instance.goal);
-                const f = g + h;
-                instance.open.push({position: neighbor, g: g, f: f, parent: current});
-            }
-        }
-    }
+const tileConfigurations = {
+    standard: {"grass": {r: 1, s: 0.025}, "stone": {r: 0, s: 0, drownDamage: 999}, "sand": {r: 0, s: 0.020}, "shallowwater": {r: 5, s: 0.0125, drownDamage: 0.025}, "deepwater": {r: 100, s: 0.0025, drownDamage: 1}, "lava": {r: 1000, s: 0.00125, drownDamage: 3}},
 }
 
-class PathManager {
-    static developingPaths = new Map();
-    static add(instance) {
-        const startX = Math.floor(instance.x);
-        const startY = Math.floor(instance.y);
-        const goalX = Math.floor(instance.target.x);
-        const goalY = Math.floor(instance.target.y);
-        PathManager.developingPaths.set(instance, new AStar([startX, startY], [goalX, goalY], instance.tileProperties));
+const Soul = {
+    "warrior":  {
+        texture: "warrior",
+        sizeX: 0.5,
+        sizeY: 0.5,
+        maxHealth: 100,
+        isGood: true, 
+        pathConfigType: "standard"},
+    "goblin":  {
+        texture: "goblin",
+        sizeX: 0.5,
+        sizeY: 0.5,
+        maxHealth: 100,
+        isGood: false, 
+        pathConfigType: "standard"},
+}
+
+class FlowField {
+
+    static getStartUnits(isGood) {
+        const start = [];
+        for (const instance of (isGood ? Creature.goodInstances : Creature.badInstances)) {
+            start.push([instance.x, instance.y]);
+        }
+        return start;
     }
 
-    static developPaths(minSteps) {
-        if (PathManager.developingPaths.size <= 0) {return};
+    static getNeighbors(point) {
 
-        let stepsLeft = minSteps;
-        let allPathsComplete = true;
-        do {
-            allPathsComplete = true;
-            for (const path of PathManager.developingPaths.values()) {
-                if (!path.finished) {
-                    AStar.step(5, path);
-                    stepsLeft -= 5;
-                }
+    }
 
-                if (!path.finished) {
-                    allPathsComplete = false;
+    static makeFlowFields() {
+        let flowField = new Map();
+        for (let p = 0; p < 2; p++) {
+            const usePathTypes = (p == 0 ? Creature.badPathTypes : Creature.goodPathTypes);
+            for (const pathType of usePathTypes.keys()) {
+                const openData = FlowField.getStartUnits(!isGood);
+                const closedData = new Set();
+                while (openData.length > 0) {
+                    const closed = 
                 }
             }
-            if (allPathsComplete) {break};
-        } while (stepsLeft > 0)
+        }
     }
 }
 
@@ -317,25 +248,22 @@ class PathManager {
 class Creature {
     static goodInstances = new Set();
     static badInstances = new Set();
+
+    static goodPathTypes = new Map();
+    static badPathTypes = new Map();
+
     static allCords = new Map();
 
     x = 0;
     y = 0;
     fluidX = 0;
     fluidY = 0;
+
+    pathConfigType = null;
     width = 0;
     height = 0;
     health = 0;
     isGood = false;
-
-    initialEnemyX = 0;
-    initialEnemyY = 0;
-    target = null;
-    pathIndex = 0;
-    path = null;
-    destination = null;
-    requestingPath = false;
-    tileProperties = null;
 
     repathRange = 1;
     attackRange = 2;
@@ -344,192 +272,44 @@ class Creature {
     returnCode = 0;
     debugInfo = null;
 
-    constructor(x, y, width, height, texture, health, isGood, tileProperties) {
+    constructor(x, y, soulType) {
         this.x = x;
         this.y = y;
         this.fluidX = x;
         this.fluidY = y;
-        this.width = width;
-        this.height = height;
-        this.texture = texture;
-        this.health = health;
-        this.isGood = isGood;
-        this.tileProperties = tileProperties;
-        const useSet = (isGood ? Creature.goodInstances : Creature.badInstances);
+
+        const info = Soul.soulType;
+        this.width = info.sizeX;
+        this.height = info.sizeY;
+        this.texture = info.texture;
+        this.health = info.maxHealth;
+        this.isGood = info.isGood;
+
+        this.pathConfigType = soulType.pathConfigType;
+        const usePathTypes = (this.isGood ? Creature.goodPathTypes : Creature.badPathTypes);
+        usePathTypes.set(soulType.pathConfigType, (usePathTypes.has(pathConfigType) ? usePathTypes.get(pathConfigType) + 1: 1));    
+        
+        const useSet = (this.isGood ? Creature.goodInstances : Creature.badInstances);
         useSet.add(this);
+        Creature.allCords.set(x+","+y, this);
     }
 
-    static getTarget(instance) {
-        const opposingSide = !instance.isGood;
-        let selectedEnemy = null;
-        let shortestDistance = Number.MAX_VALUE;
-        for (const enemy of (opposingSide ? Creature.goodInstances : Creature.badInstances)) {
-            const distance = getDistance(enemy.y, instance.y, enemy.x, instance.x);
-            if (shortestDistance > distance && enemy.health > 0) {
-                shortestDistance = distance;
-                selectedEnemy = enemy;
-            }
-        }
-        return selectedEnemy;
-    }
-/*
-overlapping issue is visual only and is a result of fluidX and fluidY being malformed.
-fixing the issue will require a rework of fluidX and fluidY
-*/
-    static act(instance) {
-        const initialPositionCord = Math.floor(instance.x) + ',' + Math.floor(instance.y);
-        Creature.allCords.set(initialPositionCord, instance);
-
+    static terminate(instance) {
         if (instance.health <= 0) {
-            Creature.allCords.delete(initialPositionCord);
-            PathManager.developingPaths.delete(instance);
-
-            instance.target = null;
-            instance.path = null;
-            instance.requestingPath = false;
-
-            for (const other of [...Creature.goodInstances, ...Creature.badInstances]) {
-                if (other.target === instance) {
-                    other.target = null;
-                    other.requestingPath = false;
-                }
+            const usePathTypes = (instance.isGood ? Creature.goodPathTypes : Creature.badPathTypes);
+            usePathTypes.set(instance.pathConfigType, usePathTypes.get(instance.pathConfigType) - 1);
+            if (usePathTypes.get(instance.pathConfigType) <= 0) {
+                usePathTypes.delete(instance.pathConfigType);
             }
-
+            Creature.allCords.delete(instance.x+","+instance.y);
             const instanceType = instance.isGood ? Creature.goodInstances : Creature.badInstances;
             instanceType.delete(instance);
-
-            instance.returnCode = 1;
-            return;
         }
-
-        // If target is far from original spot make new path to same target
-        if (instance.target) {
-            if (getDistance(instance.target.y, instance.initialEnemyY, instance.target.x, instance.initialEnemyX) > instance.repathRange) {
-                instance.target = this.getTarget(instance);
-                instance.path = null;
-            } else if (getDistance(instance.target.y, instance.y, instance.target.x, instance.x) < instance.attackRange) {
-                instance.target.health -= 1;
-                instance.returnCode = 2;
-                return;
-            }
-        }
-
-        // Find a target
-        if (!instance.requestingPath) {
-            if (!instance.target || instance.target && instance.target.health <= 0) {
-                instance.target = Creature.getTarget(instance);
-                if (instance.target) {
-                    instance.initialEnemyX = instance.target.x;
-                    instance.initialEnemyY = instance.target.y;
-                }
-                instance.returnCode = 3;
-                return;
-            }
-
-            // Move along path
-            if (instance.path && instance.pathIndex < instance.path.length) {
-                if (!instance.destination) {
-                    instance.destination = instance.path[instance.pathIndex];
-                }
-
-                const nextCord = instance.destination[0]+","+instance.destination[1];
-                const nextTileTaken = Creature.allCords.has(nextCord);
-                const nextTileInstance = (nextTileTaken ? Creature.allCords.get(nextCord) : null);
-                if (nextTileInstance && nextCord != initialPositionCord) {
-                    instance.debugInfo = "Next is blocked" + nextTileInstance.x + "," + nextTileInstance.y;
-                    instance.returnCode = 5.5;
-                    return;
-                }
-
-                const speed = instance.tileProperties[BM.map[instance.y][instance.x]].s;
-
-                const dx = instance.destination[0] - instance.fluidX;
-                const dy = instance.destination[1] - instance.fluidY;
-                const distance = Math.sqrt(dx*dx + dy*dy);
-
-                if (distance == 0) {
-                    instance.pathIndex += 1;
-                    instance.destination = null;
-                } else {
-                    const step = Math.min(speed, distance);
-                    const stepX = (dx / distance) * step;
-                    const stepY = (dy / distance) * step;
-
-                    let newX = instance.fluidX + stepX;
-                    let newY = instance.fluidY + stepY;
-                    const forcastedTile = Math.floor(newX) + ',' + Math.floor(newY);
-                    if (Creature.allCords.has(forcastedTile) && forcastedTile !== initialPositionCord) {
-                        instance.debugInfo = "Forecast blocked just now";
-                        instance.returnCode = 5.6;
-                        return;
-                    }
-                    
-                    if (forcastedTile == nextCord) {
-                        Creature.allCords.delete(initialPositionCord);
-                        Creature.allCords.set(forcastedTile, instance);
-                        instance.x = Math.floor(newX);
-                        instance.y = Math.floor(newY);
-                        instance.pathIndex++;
-                        instance.destination = null;
-                    }
-                    instance.fluidX = newX;
-                    instance.fluidY = newY;
-                }
-
-                instance.returnCode = 6;
-                return;
-            }
-        }
-
-        // Make New Path
-        if (!instance.requestingPath) {
-            instance.requestingPath = true;
-            instance.path = null;
-
-            if (instance.target) {
-                instance.initialEnemyX = instance.target.x;
-                instance.initialEnemyY = instance.target.y;
-            }
-
-            PathManager.add(instance);
-        }
-        if (PathManager.developingPaths.get(instance).finished) {
-            instance.path = PathManager.developingPaths.get(instance).path;
-            if (instance.path && instance.path.length > 1 && instance.path[0][0] === instance.x && instance.path[0][1] === instance.y) {
-                instance.path.shift(); // remove current tile from the path
-            }
-            instance.pathIndex = 0;
-            instance.destination = null;
-            PathManager.developingPaths.delete(instance);
-            instance.requestingPath = false;
-        }
-        instance.returnCode = 7;
-        return;
-
     }
 
-    static debugInstance(instance, color) {
-        const baseTileSize = WP.windowWidth / BM.maxColumns;
-        const tileSize = baseTileSize * BM.zoom;
-        const size = Math.ceil(tileSize);
-
-        const halfWidth = WP.windowWidth / 2;
-        const halfHeight = WP.windowHeight / 2;
-
-        const screenX = Math.floor((instance.fluidX - BM.mouseX + 0.5) * tileSize + halfWidth);
-        const screenY = Math.floor((instance.fluidY - BM.mouseY + 0.5) * tileSize + halfHeight);
-        const screenWidth = size*instance.width;
-        const screenHeight = size*instance.height;
-
-        ctx.beginPath();
-        ctx.fillStyle = color;
-        ctx.rect(screenX - screenWidth/2,
-            screenY - screenHeight/2,
-            screenWidth,
-            screenHeight);
-        ctx.fill();
-        ctx.closePath();
-    } 
+    static act(instance) {
+        
+    }
 
     static renderInstances() {
         const baseTileSize = WP.windowWidth / BM.maxColumns;
@@ -683,13 +463,10 @@ function bootGame() {
         }
     }
 
-    //BM.map[25][25] = "lava"
-    // new Creature(0, 0, 0.5, 0.5, "warrior", 100, true, {"grass": {r: 1, s: 0.025}, "stone": {r: 0, s: 0, drownDamage: 999}, "sand": {r: 0, s: 0.020}, "shallowwater": {r: 5, s: 0.0125}, "deepwater": {r: 25, s: 0}, "lava": {r: 25, s: 0}});
-    // new Creature(49, 49, 0.5, 0.5, "goblin", 100, false, {"grass": {r: 1, s: 0.025}, "stone": {r: 0, s: 0, drownDamage: 999}, "sand": {r: 0, s: 0.020}, "shallowwater": {r: 5, s: 0.0125}, "deepwater": {r: 25, s: 0}, "lava": {r: 25, s: 0}});
     for (let i = 0; i < 50; i++) {
         for (let o = 0; o < 10; o++) {
-            new Creature(i, 49-o, 0.5, 0.5, "warrior", 100, true, {"grass": {r: 1, s: 0.025}, "stone": {r: 0, s: 0, drownDamage: 999}, "sand": {r: 0, s: 0.020}, "shallowwater": {r: 5, s: 0.0125, drownDamage: 0.025}, "deepwater": {r: 100, s: 0.0025, drownDamage: 1}, "lava": {r: 1000, s: 0.00125, drownDamage: 3}});
-            new Creature(i, o, 0.5, 0.5, "goblin", 100, false, {"grass": {r: 1, s: 0.025}, "stone": {r: 0, s: 0, drownDamage: 999}, "sand": {r: 0, s: 0.020}, "shallowwater": {r: 5, s: 0.0125, drownDamage: 0.025}, "deepwater": {r: 100, s: 0.0025, drownDamage: 1}, "lava": {r: 1000, s: 0.00125, drownDamage: 3}});
+            new Creature(i, 49-o, "warrior");
+            new Creature(i, o, "goblin");
         }
     }
 }
@@ -757,6 +534,7 @@ function gameLoop() {
     PathManager.developPaths(2000);
 
     // Creatures
+    FlowField.makeFlowFields();
     const allCreatures = [...Creature.goodInstances, ...Creature.badInstances];
     for (let i = allCreatures.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -839,32 +617,9 @@ function gameLoop() {
             // Debug Creatures
             if (Creature.allCords.has(x+","+y)) {
                 const instance = Creature.allCords.get(x+","+y);
-                Creature.debugInstance(instance, 'blue');
                 ctx.fillStyle = "blue";
                 ctx.font = "35px serif";
                 ctx.fillText(`Cords (x, y): ${x+","+y}`, 100, 100);
-
-                const selectedCords = instance.x+","+instance.y;
-                ctx.fillText(`Instance Cords (x, y): ${selectedCords} | Instance Position (y, x): ${instance.y}, ${instance.x}`, 100, 130);
-
-                ctx.fillText(`Target: ${instance.target}`, 100, 160);
-                if (instance.target) {
-                    Creature.debugInstance(instance.target, 'orange');
-                }
-                ctx.fillStyle = "blue";
-
-                ctx.fillText(`Requesting Path: ${instance.requestingPath}`, 100, 200);
-                ctx.fillText(`Has Path: ${instance.path != null}`, 100, 230);
-                if (instance.path) {
-                    for (let i = 0; i < instance.path.length; i++) {
-                        BM.renderDebugTile(instance.path[i][0], instance.path[i][1], i);
-                    }
-                }
-
-                ctx.fillStyle = "blue";
-                ctx.font = "35px serif";
-                ctx.fillText(`Return Code ${instance.returnCode}`, 100, 270);
-                ctx.fillText(`Debug Info ${instance.debugInfo}`, 100, 300);
             } else {
             ctx.fillStyle = "orange";
             ctx.font = "25px serif";
