678cf9ea2198bc1c320ad9ad0272fbd3acfe7104
RealRottale1 <camarkham2@gmail.com>
2025-10-20 09:08:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,93 +1,29 @@
 /* Name: Christopher Markham
- * Date: 10/13/2025
- * Project Name: Account Class
- * Description: An account class which handles finacial transactions
+ * Date: 10/20/2025
+ * Project Name: BigInteger Practice
+ * Description: Calculates combinations from two numbers
  */
 
-import java.util.Date;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Main {
 
-    public static  class Account {
-        private int id;
-        private double balence;
-        private double annualInterestRate;
-        private Date dateCreated;
-
-        /* Constructor method for object without any predefined data */
-        public Account() {
-            this.id = 0;
-            this.balence = 0.0;
-            this.annualInterestRate = 0.0;
-            this.dateCreated = new Date();
+    public static BigInteger getNumberOfCombinations(int n, int k) {
+        BigInteger numerator = BigInteger.ONE;
+        BigInteger denominator = BigInteger.ONE;
+        for (int i = 0; i < k; i++) {
+            numerator = numerator.multiply(BigInteger.valueOf(n - i));
+            denominator = denominator.multiply(BigInteger.valueOf(i + 1));
         }
-
-        /* Constructor method for objects with predefined id and balence */
-        public Account(int id, double balence, double annualInterestRate) {
-            this.id = id;
-            this.balence = balence;
-            this.annualInterestRate = annualInterestRate;
-            this.dateCreated = new Date();
-        }
-
-        /* Accessor methods */
-        public int getId() {
-            return this.id;
-        }
-        public double getBalence() {
-            return this.balence;
-        }
-        public double getAnnualInterestRate() {
-            return this.annualInterestRate;
-        }
-        public Date getDateCreated() {
-            return this.dateCreated;
-        }
-
-        /* Mutator methods */
-        public void setId(int id) {
-            this.id = id;
-        }
-        public void setBalence(double balence) {
-            this.balence = balence;
-        }
-        public void setAnnualInterestRate(double annualInterestRate) {
-            this.annualInterestRate = annualInterestRate;
-        }
-
-        /* Method to get monthly interest rate */
-        public double getMonthlyInterestRate() {
-            return (this.annualInterestRate / 100) / 12;
-        }
-
-        /* Withdraw method that withdraws unless amount is greater than current balence */
-        public double withdraw(double amount) {
-            if (amount > this.balence) {
-                return 0;
-            }
-            this.balence -= amount;
-            return amount;
-        }
-
-        /* Deposit method which adds amount to current balence */
-        public void deposit(int amount) {
-            this.balence += amount;
-        }
+        return numerator.divide(denominator);
     }
-
     public static void main(String[] args) {
-        /* We create the object here */
-        Account christopher = new Account(1122, 20000, 4.5);
-        
-        /* We withdraw money here */
-        double housePayment = christopher.withdraw(2500);
-
-        /* We deposit money here */
-        christopher.deposit(3000);
-
-        /* We get output here */
-        System.out.println("Balence: "+christopher.getBalence());
-        System.out.println("Monthly Interest: "+christopher.getMonthlyInterestRate());
-        System.out.println("Date Created: "+christopher.getDateCreated());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n = scanner.nextInt();
+        System.out.println("Enter k:");
+        int k = scanner.nextInt();
+        System.out.println("The C(n, k) is "+getNumberOfCombinations(n, k));
     }
 }
