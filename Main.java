8eeb7d20d03cca2c3782e9fa4059ae3ed7f9be65
RealRottale1 <camarkham2@gmail.com>
2025-12-02 13:08:07 -0500

    _

--- Main.java
+++ Main.java
@@ -53,9 +53,7 @@ abstract class GameHandler {
 
 class War extends GameHandler {
     ArrayList<Card> player1Deck;
-    ArrayList<Card> player1Used;
     ArrayList<Card> player2Deck;
-    ArrayList<Card> player2Used;
 
     @Override
     void setupDeck() {
@@ -73,13 +71,65 @@ class War extends GameHandler {
         int middleIndex = deck.size()/2;
         player1Deck = new ArrayList<>(deck.subList(0, middleIndex));
         player2Deck = new ArrayList<>(deck.subList(middleIndex, deck.size()));
-        player1Used = new ArrayList<>();
-        player2Used = new ArrayList<>();
     }
 
     @Override
     void playGame() {
-        
+        ArrayList<Card> winPool = new ArrayList<>();
+        int useIndex = 0;
+        while (true) do {
+            // If you run out of cards you lose
+            if (useIndex != 0) {
+                int p1Size = player1Deck.size();
+                int p2Size = player2Deck.size();
+                if (p1Size == p2Size && p1Size < 3) {
+                    // Stale mate
+                    break;
+                } else if (p1Size < 3) {
+                    // Player 2 Wins
+                    break;
+                } else if (p2Size < 3) {
+                    // Player 1 Wins
+                    break;
+                }
+            }
+
+            Card player1Card = player1Deck.get(useIndex);
+            Card player2Card = player2Deck.get(useIndex);
+            System.out.println("Player1 drew "+ player1Card.faceId + player1Card.suit);
+            System.out.println("Player2 drew "+ player2Card.faceId + player2Card.suit);
+            player1Deck.remove(useIndex);
+            player2Deck.remove(useIndex);
+            winPool.add(player1Card);
+            winPool.add(player2Card);
+
+            for (int i = useIndex - 1; i > 0; i--) {
+                Card card1 = player1Deck.get(i);
+                Card card2 = player2Deck.get(i);
+                player1Deck.remove(i);
+                player2Deck.remove(i);
+                winPool.add(player1Card);
+                winPool.add(player2Card);
+            }
+
+            if (player1Card.value > player2Card.value) {
+                System.out.println("Player1 won the battle and won " + winPool.size() + "card(s)");
+                winPool.shuffle();
+                for (Card c : winPool) {
+                    player1Deck.add(c);
+                }
+                break;
+            } else if (player1Card.value < player2Card.value) {
+                                System.out.println("Player2 won the battle and won " + winPool.size() + "card(s)");
+                winPool.shuffle();
+                for (Card c : winPool) {
+                    player2Deck.add(c);
+                }
+                break;
+            } else {
+                useIndex = 2
+            }
+        }
     }
 }
 
