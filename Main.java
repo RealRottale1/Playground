71d67d6679a3cb9500e67394153c506e0e6b7e9c
RealRottale1 <camarkham2@gmail.com>
2025-12-02 10:17:06 -0500

    Working on OUR java final project ALONE

--- Main.java
+++ Main.java
@@ -1,29 +1,126 @@
-/* Name: Christopher Markham
- * Date: 10/20/2025
- * Project Name: BigInteger Practice
- * Description: Calculates combinations from two numbers
- */
-
-import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+
+class Card {
+    String suit = "";
+    String faceId = "";
+    int id = 0;
+    int value = 0;
+    Card(String suit, int id, int value) {
+        this.suit = suit;
+        this.id = id;
+        this.value = value;
+        switch (id) {
+            case 0:
+                this.faceId = "A";
+                break;
+            case 1:
+                this.faceId = "2";
+                break;
+            case 2:
+                this.faceId = "3";
+                break;
+            case 3:
+                this.faceId = "4";
+                break;
+            case 4:
+                this.faceId = "5";
+                break;
+            case 5:
+                this.faceId = "6";
+                break;
+            case 6:
+                this.faceId = "7";
+                break;
+            case 7:
+                this.faceId = "8";
+                break;
+            case 8:
+                this.faceId = "9";
+                break;
+            case 9:
+                this.faceId = "10";
+                break;
+            case 10:
+                this.faceId = "J";
+                break;
+            case 11:
+                this.faceId = "Q";
+                break;
+            case 12:
+                this.faceId = "K";
+                break;
+        }
+    }
+}
+
+abstract class GameHandler {
+
+    abstract void setupDeck(); // Establishes the current deck
+
+    abstract void playGame(); // One game function/action
+
+    public static ArrayList<Card> makeDeck(Map<Integer, Integer> worthMap) {
+        ArrayList<Card> deck = new ArrayList<>();
+        for (int s = 0; s < 4; s++) {
+            String suit = (s == 0 ? "\u2665" : (s == 1 ? "\u2666" : (s == 2 ? "\u2663" : "\u2660")));
+            for (int i = 0; i < 13; i++) {
+                Card newCard = new Card(suit, i, worthMap.get(i));
+                deck.add(newCard);
+            }
+        }
+        return deck;
+    }
+}
+
+class War extends GameHandler {
+    ArrayList<Card> player1Deck;
+    ArrayList<Card> player1Used;
+    ArrayList<Card> player2Deck;
+    ArrayList<Card> player2Used;
+
+    @Override
+    void setupDeck() {
+        // Establishes worthMap, id = worth
+        Map<Integer, Integer> worthMap = new HashMap<>();
+        for (int i = 0; i < 13; i++) {
+            worthMap.put(i, i+1);
+        }
+
+        // Makes and scrambles deck
+        ArrayList<Card> deck = GameHandler.makeDeck(worthMap);
+        Collections.shuffle(deck);
+
+        // Splits deck
+        int middleIndex = deck.size()/2;
+        player1Deck = new ArrayList<>(deck.subList(0, middleIndex));
+        player2Deck = new ArrayList<>(deck.subList(middleIndex, deck.size()));
+        player1Used = new ArrayList<>();
+        player2Used = new ArrayList<>();
+    }
+
+    @Override
+    void playGame() {
+        
+    }
+}
+
+class BlackJack extends GameHandler {
+
+}
+
+class CrazyEights extends GameHandler {
+
+}
+
+class GoFish extends GameHandler {
+}
 
 public class Main {
 
-    public static BigInteger getNumberOfCombinations(int n, int k) {
-        BigInteger numerator = BigInteger.ONE;
-        BigInteger denominator = BigInteger.ONE;
-        for (int i = 0; i < k; i++) {
-            numerator = numerator.multiply(BigInteger.valueOf(n - i));
-            denominator = denominator.multiply(BigInteger.valueOf(i + 1));
-        }
-        return numerator.divide(denominator);
-    }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter n:");
-        int n = scanner.nextInt();
-        System.out.println("Enter k:");
-        int k = scanner.nextInt();
-        System.out.println("The C(n, k) is "+getNumberOfCombinations(n, k));
     }
 }
