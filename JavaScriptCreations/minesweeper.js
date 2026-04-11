d27436daf57a4f94010585f796893c74c8c738ce
RealRottale1 <camarkham2@gmail.com>
2026-04-11 17:03:53 -0400

    _

--- JavaScriptCreations/minesweeper.js
+++ JavaScriptCreations/minesweeper.js
@@ -75,6 +75,18 @@ async function wait(time) {
     })
 }
 
+function getNeighborData(y, x) {
+    const nearbyMineNumbers = [];
+    let possibleMineSpots = 0;
+    let knownMineSpots = 0;
+    for (let i = 0; i < currentPieces[y][x][1].length; i++) {
+        const tileResult = getTile(currentPieces[y][x][1][i][0], currentPieces[y][x][1][i][1]);
+        if (tileResult == 'b') {knownMineSpots++;}
+        if (tileResult == '_') {possibleMineSpots++;}
+        nearbyMineNumbers.push(tileResult);
+    }
+    return [nearbyMineNumbers, possibleMineSpots, knownMineSpots];
+}
 async function sweepMines() {
     let clicks = 0;
     for (let y = 0; y < boardYSize; y++) {
@@ -82,17 +94,8 @@ async function sweepMines() {
             if (currentPieces[y][x]) {
                 const nearbyMines = getMineNumber(y, x);
                 if (nearbyMines == 0) {continue;}
-                let localClicks = 0;
 
-                const nearbyMineNumbers = [];
-                let possibleMineSpots = 0;
-                let knownMineSpots = 0;
-                for (let i = 0; i < currentPieces[y][x][1].length; i++) {
-                    const tileResult = getTile(currentPieces[y][x][1][i][0], currentPieces[y][x][1][i][1]);
-                    if (tileResult == 'b') {knownMineSpots++;}
-                    if (tileResult == '_') {possibleMineSpots++;}
-                    nearbyMineNumbers.push(tileResult);
-                }
+                const [nearbyMineNumbers, possibleMineSpots, knownMineSpots] = getNeighborData(y, x);
 
                 if (possibleMineSpots == 0) {continue;}
 
@@ -101,7 +104,7 @@ async function sweepMines() {
                         if (nearbyMineNumbers[i] == '_') {
                             const targetTile = currentPieces[currentPieces[y][x][1][i][0]][currentPieces[y][x][1][i][1]][0];
                             simulateClick(targetTile, false);
-                            localClicks++;
+                            clicks++;
                         }
                     }
                 } else if ((knownMineSpots + possibleMineSpots) == nearbyMines) {
@@ -109,37 +112,69 @@ async function sweepMines() {
                         if (nearbyMineNumbers[i] == '_') {
                             const targetTile = currentPieces[currentPieces[y][x][1][i][0]][currentPieces[y][x][1][i][1]][0];
                             simulateClick(targetTile, true);
-                            localClicks++;
+                            clicks++;
                         }
                     }
                 }
-
-                // Pattern recognition
-                if (localClicks == 0 && possibleMineSpots != 0) {
-                    /*
-                        - B -
-                        1 2 1
-                        - B -
-
-                        - B B -
-                        1 2 2 1
-                        _ _ _ _
-                    */
-                }
-                
-                clicks += localClicks;
             }
         }
     }
     return clicks;
 }
 
+async function heavySweepMines() {
+    let currentPiecesRisk = {};
+    for (let y = 0; y < boardYSize; y++) {
+        for (let x = 0; x < boardXSize; x++) {
+            if (currentPieces[y][x]) {
+                const nearbyMines = getMineNumber(y, x);
+                if (nearbyMines == 0) {continue;}
+
+                const [nearbyMineNumbers, possibleMineSpots, knownMineSpots] = getNeighborData(y, x);
+            
+                if (possibleMineSpots == 0) {continue;}
+                const risk = (nearbyMines - knownMineSpots) / possibleMineSpots;
+                for (let i = 0; i < currentPieces[y][x][1].length; i++) {
+                    if (nearbyMineNumbers[i] == '_') {
+                        const neighborPiece = currentPieces[y][x][1][i];
+                        const neighborY = neighborPiece[0];
+                        const neighborX = neighborPiece[1]
+                        if (!currentPiecesRisk[neighborY]) {
+                            currentPiecesRisk[neighborY] = {};
+                        }
+                        if (!currentPiecesRisk[neighborY][neighborX]) {
+                            currentPiecesRisk[neighborY][neighborX] = risk;
+                        } else {
+                            currentPiecesRisk[neighborY][neighborX] += risk;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    const leastRiskyPiece = [-1 ,-1];
+    let leastRisk = Infinity;
+    for (const y in currentPiecesRisk) {
+        for (const x in currentPiecesRisk[y]) {
+            const risk = currentPiecesRisk[y][x];
+            if (risk < leastRisk) {
+                leastRiskyPiece[0] = y;
+                leastRiskyPiece[1] = x;
+                leastRisk = risk;
+            }
+        }
+    }
+    if (leastRiskyPiece[0] == -1) {return false;}
+    const targetTile = currentPieces[leastRiskyPiece[0]][leastRiskyPiece[1]][0];
+    simulateClick(targetTile, false);
+}
+
 simulateClick(currentPieces[Math.floor(boardYSize/2)][Math.floor(boardXSize/2)][0], false);
 do {
     const clicks = await sweepMines();
     if (clicks == 0) {
-        await wait(500);
-        console.log("Awaiting!");
+        heavySweepMines();
     }
     await wait(100);
 } while (!document.getElementsByClassName("facewin").length);
