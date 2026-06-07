385a606154dbebb9522f067c22e0f3d548a1952c
RealRottale1 <camarkham2@gmail.com>
2026-06-07 09:52:41 -0400

    _

---
+++ UNCCharlottePrep/BinarySearchTree.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Node {
+    private int value;
+    private Node lNode;
+    private Node rNode;
+    public Node(int value) {
+        this.value = value;
+    }
+    public int getValue() {}
+}
+
+class BinarySearchTree {
+
+}
