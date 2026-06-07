c104a371abec710ee91d3d715a1fbb6074dc4726
RealRottale1 <camarkham2@gmail.com>
2026-06-07 09:02:54 -0400

    Finsihed for now

--- JavaCreations/MazeAlgorithm.java
+++ JavaCreations/MazeAlgorithm.java
@@ -1,34 +1,30 @@
+import java.util.Map;
 import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
 
+enum AlgorithmType {BFS, DIJKSTRAS}
 record Pair<k, v>(k key, v value){}
 
 public class Main {
 
 
     public static void main(String[] args) {
-        // Maze testMaze = new Maze("0,0,1,0/1,0,1,0/0,0,0,0/0,0,1,0", true);
-        // System.out.println("Created Maze Network!");
-        // List<String> solution = BFSSolver.solve(testMaze, "0,0", "0,3");
-        // System.out.println("Finished!");
-        // System.out.println(solution);
-
-        Maze testMaze2 = new Maze("0,0,1,0/1,0,1,0/0,0,0,0/0,0,1,0", false);
+        Maze testMaze = new Maze("0,0,1,0/1,0,1,0/0,0,0,0/0,0,1,0", AlgorithmType.BFS);
         System.out.println("Created Maze Network!");
-        List<String> solution2 = DijkstraSolver.solve(testMaze2, "0,0", "0,3");
+        List<String> solution = BFSSolver.solve(testMaze, "0,0", "0,3");
+        System.out.println("Finished!");
+        System.out.println(solution);
+
+        Maze testMaze2 = new Maze("0,0,1,0/1,1,1,0/0,0,2,0/0,0,1,0", AlgorithmType.DIJKSTRAS);
+        System.out.println("Created Maze Network!");
+        List<String> solution2 = DijkstraSolver.solve(testMaze2, "0,0", "1,3");
         System.out.println("Finished!");
         System.out.println(solution2);
     }
-
-    /*
-       "S,0,1,0/
-        1,0,1,E/
-        0,0,0,0/
-        0,0,1,0"
-    */
 }
 
 class MazeNode {
@@ -44,7 +40,20 @@ class MazeNode {
 class Maze {
     HashMap<String, MazeNode> MapNetwork;
 
-    public Maze(String rawData, boolean isBFS) {
+    static List<String> reconstruct(Maze unsolvedMaze, String endNode, List<String> solvedMaze) {
+        // Converts the completed network into a linear path
+        String lastNode = endNode;
+        do {
+            solvedMaze.add(lastNode);
+            if (unsolvedMaze.MapNetwork.get(lastNode) == null || unsolvedMaze.MapNetwork.get(lastNode).Parent == null) {
+                break;
+            }
+            lastNode = unsolvedMaze.MapNetwork.get(lastNode).Parent;
+        } while (true);
+        return solvedMaze.reversed();
+    }
+
+    public Maze(String rawData, AlgorithmType useAlgorithm) {
         List<List<Pair<String, Integer>>> transformedData = new ArrayList<>();
         int xSize;
         int ySize;
@@ -71,6 +80,10 @@ class Maze {
         xSize = transformedData.get(0).size();
 
         // Transforms array list data into network data
+        int wallValue = 1; // BFS wall value by default
+        if (useAlgorithm == AlgorithmType.DIJKSTRAS) {
+            wallValue = -1;
+        }
         HashMap<String, MazeNode> MapNetwork = new HashMap<>();
         for (int y = 0; y < ySize; y++) {
             for (int x = 0; x < xSize; x++) {
@@ -78,10 +91,10 @@ class Maze {
                 Integer Weight = transformedData.get(y).get(x).value();
 
                 Set<String> Children = new HashSet<>();
-                if (y + 1 < ySize) {if (transformedData.get(y+1).get(x).value() != 1 || !isBFS) {Children.add(transformedData.get(y+1).get(x).key());}}
-                if (y - 1 >= 0)    {if (transformedData.get(y-1).get(x).value() != 1 || !isBFS) {Children.add(transformedData.get(y-1).get(x).key());}}
-                if (x + 1 < xSize) {if (transformedData.get(y).get(x+1).value() != 1 || !isBFS) {Children.add(transformedData.get(y).get(x+1).key());}}
-                if (x - 1 >= 0)    {if (transformedData.get(y).get(x-1).value() != 1 || !isBFS) {Children.add(transformedData.get(y).get(x-1).key());}}
+                if (y + 1 < ySize) {if (transformedData.get(y+1).get(x).value() != wallValue) {Children.add(transformedData.get(y+1).get(x).key());}}
+                if (y - 1 >= 0)    {if (transformedData.get(y-1).get(x).value() != wallValue) {Children.add(transformedData.get(y-1).get(x).key());}}
+                if (x + 1 < xSize) {if (transformedData.get(y).get(x+1).value() != wallValue) {Children.add(transformedData.get(y).get(x+1).key());}}
+                if (x - 1 >= 0)    {if (transformedData.get(y).get(x-1).value() != wallValue) {Children.add(transformedData.get(y).get(x-1).key());}}
 
                 MazeNode NetworkNode = new MazeNode(Weight, Children);
                 MapNetwork.put(NetworkKey, NetworkNode);
@@ -126,22 +139,62 @@ class BFSSolver {
             nextNodes = new HashSet<>();
         } while (!foundExit || currentNodes.size() <= 0);
 
-        // Converts the completed network into a linear path
         if (!foundExit) {
             return solvedMaze;
         }
-        String lastNode = endNode;
-        do {
-            solvedMaze.add(lastNode);
-            if (unsolvedMaze.MapNetwork.get(lastNode) == null || unsolvedMaze.MapNetwork.get(lastNode).Parent == null) {
-                break;
-            }
-            lastNode = unsolvedMaze.MapNetwork.get(lastNode).Parent;
-        } while (true);
-        return solvedMaze.reversed();
+        return Maze.reconstruct(unsolvedMaze, endNode, solvedMaze);
     }
 }
 
 class DijkstraSolver {
-    
+    static List<String> solve(Maze unsolvedMaze, String startNode, String endNode) {
+        List<String> solvedMaze = new ArrayList<>();
+
+        unsolvedMaze.MapNetwork.get(startNode).Weight = 0;
+        unsolvedMaze.MapNetwork.get(endNode).Weight = 0;
+        TreeMap<Integer, Set<String>> nextNodes = new TreeMap<>();
+        Set<String> starter = new HashSet<>();
+        starter.add(startNode);
+        nextNodes.put(0, starter);
+
+        Set<String> oldNodes = new HashSet<>();
+        oldNodes.add(startNode);
+        boolean foundExit = false;
+        do {
+            int lowestNodeWeight = nextNodes.firstKey(); 
+            String currentNodeKey = nextNodes.get(lowestNodeWeight).iterator().next();
+            MazeNode currentNode = unsolvedMaze.MapNetwork.get(currentNodeKey);
+            if (nextNodes.get(lowestNodeWeight).size() <= 1) {
+                nextNodes.remove(lowestNodeWeight);
+            } else {
+                nextNodes.get(lowestNodeWeight).remove(currentNodeKey);
+            }
+
+            for (String child : currentNode.Children) {
+                if (!oldNodes.contains(child)) {
+                    MazeNode childNode = unsolvedMaze.MapNetwork.get(child);
+                    childNode.Weight += currentNode.Weight;
+                    int childWeight = childNode.Weight;
+                    if (nextNodes.get(childWeight) == null) {
+                        Set<String> newSet = new HashSet<>();
+                        newSet.add(child);
+                        nextNodes.put(childWeight, newSet);
+                    } else {
+                        nextNodes.get(childWeight).add(child);
+                    }
+                    oldNodes.add(child);
+                    childNode.Parent = currentNodeKey;
+                    if (child.equals(endNode)) {
+                        foundExit = true;
+                        break;
+                    }
+                }
+            }
+        } while (!foundExit || nextNodes.size() > 0);
+
+        if (!foundExit) {
+            return solvedMaze;
+        }
+        return Maze.reconstruct(unsolvedMaze, endNode, solvedMaze);
+    }
 }
