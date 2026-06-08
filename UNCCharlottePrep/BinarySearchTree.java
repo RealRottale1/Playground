ebdb23f2ea2dad06d2f2321b48fec5c58b6c54d7
RealRottale1 <camarkham2@gmail.com>
2026-06-08 08:10:07 -0400

    _

--- UNCCharlottePrep/BinarySearchTree.java
+++ UNCCharlottePrep/BinarySearchTree.java
@@ -9,7 +9,7 @@ public class Main {
         BST.insert(85);
         BST.insert(80);
         BST.insert(75);
-        BST.remove(70);
+        BST.removeViaRecursion(70);
     }
 }
 
@@ -48,7 +48,7 @@ class Node {
 }
 
 class BinarySearchTree {
-    private Node rootNode
+    private Node rootNode;
     public BinarySearchTree(int rootValue) {
         this.rootNode = new Node(rootValue);
     }
@@ -69,7 +69,138 @@ class BinarySearchTree {
         } while (true);
     }
 
-    public void remove(int removeValue) {
-        /* Learn to code*/
+    public void removeViaIteration(int removeValue) {
+        /*
+            Key
+            No children
+                - remove the parent and the connection to it
+            Only left child
+                - Shift the left child up
+            A single right child
+                - Shift the right child up
+            Else
+                - swap the node and move its right child up to its old position
+        */
+
+       // Finds the node to remove
+       Node currentNode = rootNode;
+        do {
+            if (currentNode == null) {
+                return; // No such value exists
+            }
+            int currentNodeValue = currentNode.getValue();
+            if (currentNodeValue != removeValue) {
+                currentNode = (removeValue < currentNodeValue) ? currentNode.getLNode() : currentNode.getRNode();
+            } else {
+                break;
+            }
+        } while (true);
+
+        //Gets children of removeNode
+        Node currentParentNode = currentNode.getParentNode();
+        Node currentLNode = currentNode.getLNode();
+        Node currentRNode = currentNode.getRNode();
+
+        // Handles no children
+        if (currentLNode == null && currentRNode == null) {
+            // Handles current parent
+            if (currentParentNode != null) {
+                if (removeValue < currentParentNode.getValue()) {
+                    currentParentNode.setLNode(null);
+                } else {
+                    currentParentNode.setRNode(null);
+                }
+            } else {
+                this.rootNode = null;
+            }
+            return;
+        }
+
+        // Handles just left child
+        if (currentLNode != null && currentRNode == null) {
+            currentNode.setLNode(null);
+            currentLNode.setParentNode(currentParentNode);
+
+            // Handles current parent
+            if (currentParentNode == null) {
+                this.rootNode = currentLNode;
+            } else {
+                if (currentLNode.getValue() < currentParentNode.getValue()) {
+                    currentParentNode.setLNode(currentLNode);
+                } else {
+                    currentParentNode.setRNode(currentLNode);
+                }
+            }
+            return;
+        }
+
+        // Gets left most node (from the right child of the current node)
+        Node currentLMostNode = currentRNode;
+        do {
+            Node nextNode = currentLMostNode.getLNode();
+            if (nextNode == null) {
+                break;
+            }
+            currentLMostNode = nextNode;
+        } while (true);
+
+        // Handles just right child
+        if (currentLMostNode == currentRNode) {
+            currentNode.setRNode(null);
+            currentRNode.setParentNode(currentParentNode);
+
+            // Handles re-attaching existing leftNode
+            currentRNode.setLNode(currentNode.getLNode());
+            if (currentLNode != null) {
+                currentLNode.setParentNode(currentRNode);
+            }
+
+            // Handles current parent
+            if (currentParentNode == null) {
+                this.rootNode = currentRNode;
+            } else {
+                if (currentRNode.getValue() < currentParentNode.getValue()) {
+                    currentParentNode.setLNode(currentRNode);
+                } else {
+                    currentParentNode.setRNode(currentRNode);
+                }
+            }
+            return;
+        }
+
+        // Handles left most node
+        Node currentLMostRNode = currentLMostNode.getRNode();
+
+        // Handles moving up potential right node of currentLMostNode
+        if (currentLMostRNode != null) {
+            currentLMostRNode.setParentNode(currentLMostNode.getParentNode());
+        }
+        currentLMostNode.getParentNode().setLNode(currentLMostRNode); // Either null or the right node
+        currentLMostNode.setParentNode(currentParentNode);
+
+        // Handles current parent
+        if (currentParentNode == null) {
+            this.rootNode = currentLMostNode;
+        } else {
+            if (currentLMostNode.getValue() < currentParentNode.getValue()) {
+                currentParentNode.setLNode(currentLMostNode);
+            } else {
+                currentParentNode.setRNode(currentLMostNode);
+            }
+        }
+
+        // Finishes the re-attachment process
+        currentLMostNode.setLNode(currentNode.getLNode());
+        currentLMostNode.setRNode(currentNode.getRNode());
+        if (currentLNode != null) {
+            currentLNode.setParentNode(currentLMostNode);
+        }
+        if (currentRNode != null) {
+            currentRNode.setParentNode(currentLMostNode);
+        }
+    }
+
+    public void removeViaRecursion(int removeValue) {
+
     }
 }
