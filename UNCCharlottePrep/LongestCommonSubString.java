2788580cc67c8c850f7a00d277e2222d1036392f
RealRottale1 <camarkham2@gmail.com>
2026-06-12 17:15:11 -0400

    Oh Yeah O(N) Speed Achieved!

--- UNCCharlottePrep/LongestCommonSubString.java
+++ UNCCharlottePrep/LongestCommonSubString.java
@@ -2,12 +2,53 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LCS.findNonDP("ABCDE", "ACE"));
+        System.out.println(LCS.findUsingNSquared("APPLEASTER", "PLEASE"));
+        System.out.println(LCS.findUsingN("APPLEASTER", "PLEASE"));
     }
 }
 
 class LCS {
-    static public String findNonDP(String s1, String s2) {
+    static public String findUsingN(String s1, String s2) {
+        System.out.println(s2 + " in " + s1);
+        int s1Size = s1.length();
+        int s2Size = s2.length();
+        int lStartLength = -1;
+        int lStartIndex = -1;
+        int cI = 0;
+        int startIndex = 0;
+        char cChar = s2.charAt(0);
+        for (int i = 0; i < s1Size; i++) {
+            char iChar = s1.charAt(i);
+            if (iChar == cChar) {
+                if (cI == 0) {
+                    startIndex = i;
+                }
+                if (cI > lStartLength) {
+                    lStartLength = cI;
+                    lStartIndex = startIndex;
+                }
+                cI++;
+                if (cI >= s2Size) {
+                    break;
+                }
+                cChar = s2.charAt(cI);
+            } else {
+                if (cI != 0) {
+                    i--;
+                }
+                cI = 0;
+                cChar = s2.charAt(0);
+            }
+        }
+        if (lStartIndex >= 0) {
+            return s1.substring(lStartIndex, lStartIndex+lStartLength+1);
+        } else {
+            return "";
+        }
+    }
+
+    static public String findUsingNSquared(String s1, String s2) {
+        System.out.println(s2 + " in " + s1);
         int s1Size = s1.length();
         int s2Size = s2.length();
 
@@ -43,6 +84,9 @@ class LCS {
                     }
                 }
             } else {
+                if (cI != 0) {
+                    i--;
+                }
                 cI = 0;
             }
         }
